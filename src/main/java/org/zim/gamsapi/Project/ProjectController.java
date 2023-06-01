5f04067dec548c31189da138a443c9e28341a5cc
Fabio Tosques <tosques@BGSZ142.UNI-GRAZ.AT>
2023-06-01 14:33:43 +0200

    Native pipeline fix: .gitlab-ci.yml, pom.xml
    - add maven plugin org.graalvm.buildtools.native-maven-plugin
      The plugin version 0.9.20 works with Spring Boot 3.0.6
      Native build and start of gams-api should work.
    
    - New order in .gitlab-ci.yml pipeline: build/compile, test, deploy
    - Added @ModelAttribute Project to ProjectController @GetMapping method
      to fix IllegalStateException: No primary or single unique constructor
      found for class Project
      See: https://github.com/spring-projects/spring-framework/issues/29770
    
      Adding @ModelAttribute seems to fix the problem.

--- src/main/java/org/zim/gamsapi/Project/ProjectController.java
+++ src/main/java/org/zim/gamsapi/Project/ProjectController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.zim.gamsapi.Project.interfaces.IProjectService;
 
@@ -17,7 +18,7 @@ public class ProjectController {
   private final IProjectService projectService;
 
   @GetMapping
-  public String showProjectDigitalObjects(Model model, Project project) {
+  public String showProjectDigitalObjects(Model model, @ModelAttribute Project project) {
     Project curProject = projectService.findPlain(project);
     model.addAttribute(curProject);
     return "Project/show";
