8a2e7501342ccf723ba25620cb40c325cd9aeb32
Sebi <SebastianStoff@gmx.at>
2023-06-23 11:15:04 +0200

    fixed logging for controller redirects using the x-forwarded-host and x-forwarded-proto header
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/1

--- src/main/java/org/zim/gamsapi/System/utils/ControllerUtils.java
+++ src/main/java/org/zim/gamsapi/System/utils/ControllerUtils.java
@@ -21,17 +21,24 @@ public class ControllerUtils {
         String xForwardedHostHeader = "x-forwarded-host";
 
         if(!requestHeaders.containsKey(xForwardedHostHeader)){
-            log.debug("Found no {} header at incoming request. Resolving origin to just '/'", xForwardedHostHeader);
+            log.trace("Found no {} header at incoming request. Resolving origin to just '/'", xForwardedHostHeader);
+            return "/";
+        }
+
+        String xForwardedProtoHeader = "x-forwarded-proto";
+        if(!requestHeaders.containsKey(xForwardedProtoHeader)){
+            log.error("Found no {} header at incoming request BUT must be provided along x-forwarded-host. Resolving origin to just '/'", xForwardedProtoHeader);
             return "/";
         }
 
         String forwardedHostname = requestHeaders.get(xForwardedHostHeader);
         String forwardProtocol = requestHeaders.get("x-forwarded-proto");
         String resolvedProxyOrigin = String.format("%s://%s/",forwardProtocol, forwardedHostname);
-        log.debug("Found {} header at incoming request. Resolving origin to just '/'", xForwardedHostHeader);
+        log.trace("Found {} header with value {} at incoming request. Resolving origin to just '/'", xForwardedHostHeader, forwardedHostname);
+        log.trace("Found {} header with value {} at incoming request. Resolving origin to just '/'", xForwardedProtoHeader, forwardProtocol);
 
         // assert no null will land inside redirection
-        if(!resolvedProxyOrigin.contains("null")) log.error("Failed redirection: Incorrectly constructed origin {} containing null. Got x-forwarded-host value {} and x-forwarded-proto value {}. Both have to be supplied by a reverse-proxy.", resolvedProxyOrigin, forwardedHostname, forwardProtocol);
+        if(resolvedProxyOrigin.contains("null")) log.error("Failed redirection: Incorrectly constructed origin {} containing null. Got x-forwarded-host value {} and x-forwarded-proto value {}. Both have to be supplied by a reverse-proxy.", resolvedProxyOrigin, forwardedHostname, forwardProtocol);
 
         return resolvedProxyOrigin;
     }
