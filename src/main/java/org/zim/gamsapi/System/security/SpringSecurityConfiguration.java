dfad2d6df8466b5006faaec200530aabeff57f75
Sebi <SebastianStoff@gmx.at>
2023-10-11 15:10:02 +0200

    reconfigured security
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/39

--- src/main/java/org/zim/gamsapi/System/security/SpringSecurityConfiguration.java
+++ src/main/java/org/zim/gamsapi/System/security/SpringSecurityConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.util.MimeTypeUtils;
 
@@ -23,7 +25,9 @@ public class SpringSecurityConfiguration {
       try {
         authorize
                 .anyRequest()
-                .permitAll()
+                .authenticated()
+                .and()
+                .httpBasic()
                 .and()
                 .csrf()
                 .ignoringRequestMatchers(request -> {
@@ -43,4 +47,10 @@ public class SpringSecurityConfiguration {
     return http.build();
 
   }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
 }
