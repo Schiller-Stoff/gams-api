d49b35ef331c1ee67a881bf185a8f919940fb3fb
Sebastian Stoff <sebastianstoff@gmx.at>
2023-10-13 14:44:08 +0200

    added new methods to project interface
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/39

--- src/main/java/org/zim/gamsapi/Project/interfaces/IProjectService.java
+++ src/main/java/org/zim/gamsapi/Project/interfaces/IProjectService.java
@@ -1,11 +1,15 @@
 package org.zim.gamsapi.Project.interfaces;
 
 import org.zim.gamsapi.Project.Project;
+import org.zim.gamsapi.User.User;
 
 public interface IProjectService {
 
+  User createNewProject(Project project, User user);
 
   Project saveProject(Project project);
+
+  void deleteProject(Project project);
 
   Project findProject(String projectAbbr);
 
