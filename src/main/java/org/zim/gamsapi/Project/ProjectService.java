6f5aca598ba2ec9ef24096b511aefab2b4e59852
Sebastian Stoff <sebastianstoff@gmx.at>
2023-10-17 10:47:46 +0200

    fixed deletion of projects via removing many-to-many relations with jav code
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/39

--- src/main/java/org/zim/gamsapi/Project/ProjectService.java
+++ src/main/java/org/zim/gamsapi/Project/ProjectService.java
@@ -11,10 +11,10 @@ import org.zim.gamsapi.Project.interfaces.IProjectService;
 import org.zim.gamsapi.User.User;
 import org.zim.gamsapi.User.exceptions.UserNotFoundException;
 import org.zim.gamsapi.User.interfaces.IUserRepository;
-
-import java.util.List;
+import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -25,6 +25,7 @@ public class ProjectService implements IProjectService {
   private final IUserRepository userRepository;
 
   @Override
+  @Transactional
   public User createNewProject(Project project, User user) {
 
     Optional<Project> projectOptional = projectRepository.findById(project.getProjectAbbr());
@@ -43,11 +44,15 @@ public class ProjectService implements IProjectService {
 
     User userToSave = userOptional.get();
     // add user to project and save project
-    project.setUsers(Set.of(user));
+    project.setUsers(new HashSet<>(Set.of(user)));
     projectRepository.save(project);
+    log.debug("Create project - updated user associations {}", project);
+
     // add project to user and save user
     userToSave.getProjects().add(project);
+    userToSave.setProjects(new HashSet<>(userToSave.getProjects()));
     userToSave = userRepository.save(userToSave);
+    log.debug("Create project - updated project associations {}", userToSave);
     return userToSave;
   }
 
@@ -60,12 +65,35 @@ public class ProjectService implements IProjectService {
   @Override
   @Transactional
   public void deleteProject(Project project) {
-    projectRepository.findById(project.getProjectAbbr()).orElseThrow(() -> {
+    Project foundProject = projectRepository.findById(project.getProjectAbbr()).orElseThrow(() -> {
       String msg = String.format("Project %s not found. Cannot delete project", project.getProjectAbbr());
       log.error(msg);
       return new ProjectNotFoundException(msg);
     });
+
+    log.trace("Found project {}", foundProject);
+
+    // update users referenced in project AND projects referenced in user
+    userRepository.findAll().forEach(user -> {
+      // remove project association from user
+      Set<Project> newProjects = user.getProjects().stream()
+              .filter(assignedProject -> !assignedProject.getProjectAbbr().equals(project.getProjectAbbr()))
+              .collect(Collectors.toSet());
+      user.setProjects(new HashSet<>(newProjects));
+      userRepository.save(user);
+
+      // remove user association from project
+      Set<User> newAssignedUsers =  foundProject.getUsers().stream()
+              .filter(assignedUser -> !assignedUser.getUsername().equals(user.getUsername()))
+              .collect(Collectors.toSet());
+      foundProject.setUsers(new HashSet<>(newAssignedUsers));
+      projectRepository.save(foundProject);
+
+      //TODO add removal of digital objects too?
+    });
+
     projectRepository.delete(project);
+    log.info("Successfully deleted project {}", project);
   }
 
   @Override
