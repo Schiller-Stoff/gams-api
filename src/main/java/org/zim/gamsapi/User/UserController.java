f43e0829915511496bfbc6240e77944c4b46295c
Sebi <SebastianStoff@gmx.at>
2023-10-17 14:38:46 +0200

    allowing now to get an overview over all created users in the api
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/39

--- src/main/java/org/zim/gamsapi/User/UserController.java
+++ src/main/java/org/zim/gamsapi/User/UserController.java
@@ -1,15 +1,20 @@
 package org.zim.gamsapi.User;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.zim.gamsapi.User.interfaces.IUserService;
 
+import java.util.List;
+
 @Controller
-@RequestMapping("/api/v1/user")
+@RequestMapping("/api/v1")
+@Slf4j
 public class UserController {
 
   public IUserService userService;
@@ -19,14 +24,14 @@ public class UserController {
   }
 
 
-  @GetMapping(path = "/{username}")
+  @GetMapping(path = "/user/{username}")
   public String showUserProjects(User user, Model model){
     user = userService.findByUsername(user.getUsername());
     model.addAttribute(user);
     return "User/userprojects";
   }
 
-  @GetMapping
+  @GetMapping(path = "/user")
   public String showUserProjectsViaCredentials(HttpServletRequest request,User user, Model model){
     String authUsername = request.getRemoteUser();
     // TODO error handling here
@@ -35,11 +40,17 @@ public class UserController {
     return "User/userprojects";
   }
 
-  @PostMapping
+  @PostMapping(path = "/user")
   public String createUser(User user, Model model){
     user = userService.saveUser(user);
     model.addAttribute(user);
     return "User/userprojects";
+  }
+
+  @GetMapping(path = "/users")
+  @ResponseBody
+  public List<User> showAllUsers(){
+    return userService.findAll();
   }
 
 }
