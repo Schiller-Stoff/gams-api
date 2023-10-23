1e03e1b737d221482e5f2b7b00ee00416e269c33
Sebi <SebastianStoff@gmx.at>
2023-10-23 15:37:44 +0200

    added first implementation of custom authorization class
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/34

---
+++ src/main/java/org/zim/gamsapi/System/security/UserProjectAuthorizationManager.java
@@ -0,0 +1,69 @@
+package org.zim.gamsapi.System.security;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
+import org.springframework.stereotype.Component;
+import java.util.List;
+import java.util.function.Supplier;
+
+
+/**
+ * Handles authorization for the gams-api.
+ * Like a user must have required role and assignment to project to be able to
+ * change state of digital objects and datastreams.
+ */
+@RequiredArgsConstructor
+@Component
+@Slf4j
+public class UserProjectAuthorizationManager implements AuthorizationManager<RequestAuthorizationContext> {
+
+  @Override
+  public AuthorizationDecision check(Supplier<Authentication> authentication, RequestAuthorizationContext authorizationContext) {
+
+    String requestMethod = authorizationContext.getRequest().getMethod();
+    String requestUri = authorizationContext.getRequest().getRequestURI();
+
+    log.trace("Checking custom authorization process...");
+
+    // all GET requests are being authorized
+    if(requestMethod.equals(HttpMethod.GET.name())){
+      log.debug("ACCESS GRANTED - GET requests are not protected for url {}", requestUri);
+      return new AuthorizationDecision(true);
+    }
+
+    // (if user is actually available is already done via authentication workflow (UserDetailsService)
+
+    // access authorities from authentication workflow
+    List<String> userAuthorities = authentication.get().getAuthorities().stream().map(GrantedAuthority::getAuthority).toList();
+    String username = authorizationContext.getRequest().getRemoteUser();
+
+    // administrator is allowed to do everything
+    if(userAuthorities.contains(SecurityRoles.ADMINISTRATOR.name)) {
+      log.debug("ACCESS GRANTED for User {} with role '{}' to {} with {}", username, SecurityRoles.ADMINISTRATOR.name, requestUri, requestMethod);
+      return new AuthorizationDecision(true);
+    }
+
+    // check if user is assigned to project
+    String projectAbbr = authorizationContext.getVariables().get("projectAbbr"); //defined in request matcher in SpringSecurityConfiguration.java
+    if(userAuthorities.contains(projectAbbr)){
+      // grant access only if assigned project AND editor role.
+      if(userAuthorities.contains(SecurityRoles.EDITOR.name)){
+        log.debug("ACCESS GRANTED - User {} is authorized for project {} and has required {} role. Url: {} Method: {}", username, projectAbbr, SecurityRoles.EDITOR.name, requestUri, requestMethod);
+        return new AuthorizationDecision(true);
+      } else {
+        log.debug("ACCESS DENIED - User {} has access to project {} BUT is missing the required {} role. Url: {}, Method: {}", username, projectAbbr, SecurityRoles.EDITOR.name, requestUri, requestMethod);
+        return new AuthorizationDecision(false);
+      }
+    } else {
+      log.debug("ACCESS DENIED - User {} is not authorized for project {}. Url: {}, Method: {}", username, projectAbbr, requestUri, requestMethod);
+      return new AuthorizationDecision(false);
+    }
+  }
+
+}
