f3c7bd281449bae95d57f669f03e5871e51c4e97
Sebi <SebastianStoff@gmx.at>
2023-10-31 09:17:11 +0100

    opening up solr endpoints for JSON queries
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/16

--- src/main/java/org/zim/gamsapi/Integration/BaseSearch/BaseSearchProxyController.java
+++ src/main/java/org/zim/gamsapi/Integration/BaseSearch/BaseSearchProxyController.java
@@ -5,10 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.zim.gamsapi.Integration.IIntegrationProxyController;
 import org.zim.gamsapi.System.IProxyService;
 import org.zim.gamsapi.System.configproperties.GAMSDockerDNS;
@@ -26,7 +23,7 @@ public class BaseSearchProxyController implements IIntegrationProxyController {
   private final IProxyService proxyService;
   private final GAMSDockerDNS gamsConfigProperties;
 
-  @GetMapping("/**")
+  @RequestMapping(value = "/**", method = {RequestMethod.GET, RequestMethod.POST})
   public ResponseEntity<String> proxy(
           @RequestBody(required = false) String body,
           HttpServletRequest request
