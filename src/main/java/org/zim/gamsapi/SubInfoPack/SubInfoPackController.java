c5366efccc0181b6fe09d0aae27d4a507b58cbac
Sebi <SebastianStoff@gmx.at>
2023-11-10 11:53:09 +0100

    increased tolerance of spring applications to receive multipart requests (python urllib requests fail otherwise )
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/47

--- src/main/java/org/zim/gamsapi/SubInfoPack/SubInfoPackController.java
+++ src/main/java/org/zim/gamsapi/SubInfoPack/SubInfoPackController.java
@@ -1,10 +1,12 @@
 package org.zim.gamsapi.SubInfoPack;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.Part;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import org.zim.gamsapi.SubInfoPack.exceptions.SubInfoPackProcessingException;
 import org.zim.gamsapi.SubInfoPack.interfaces.ISubInfoPackService;
 import java.io.IOException;
@@ -19,15 +21,22 @@ public class SubInfoPackController {
 
   @PostMapping(produces = "application/json", path = {"/api/v1/projects/{projectAbbr}/objects/", "/api/v1/projects/{projectAbbr}/objects"})
   @ResponseBody
-  public void ingestSubInfoPack(@ModelAttribute SubInfoPack subInfoPack, @RequestParam MultipartFile subInfoPackZIP){
+  public void ingestSubInfoPack(@ModelAttribute SubInfoPack subInfoPack, HttpServletRequest request) {
 
     byte[] sipAsZIP;
+    final String ZIP_FORM_PART_NAME = "subInfoPackZIP";
     try {
-      sipAsZIP = subInfoPackZIP.getBytes();
+      Part subInfoPackZIPPart = request.getPart(ZIP_FORM_PART_NAME);
+      sipAsZIP = subInfoPackZIPPart.getInputStream().readAllBytes();
     } catch (IOException e){
-      String msg = String.format("Cannot retrieve bytes of ingest operation for SIP: %s", subInfoPack);
+      String msg = String.format("Failed to read given zip-file via multipart form-data request for subInfoPack: %s", subInfoPack);
       log.error(msg);
       throw new SubInfoPackProcessingException(msg);
+    } catch (ServletException e){
+      String msg = String.format("Failed to extract form part: %s from multipart request against %s. There might be ", ZIP_FORM_PART_NAME, request.getRequestURI());
+      log.error(msg);
+      throw new SubInfoPackProcessingException(msg);
+
     }
     subInfoPack.setZippedFolder(sipAsZIP);
     subInfoPackService.ingest(subInfoPack);
