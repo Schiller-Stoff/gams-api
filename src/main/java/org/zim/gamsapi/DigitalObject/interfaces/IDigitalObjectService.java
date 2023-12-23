7a423b7c3f6bb4cd7f687935f55fff83fc045132
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2023-12-23 20:12:56 +0100

    added service layer interface method to allow to assign arbitrary child objects to an existing object
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/69

--- src/main/java/org/zim/gamsapi/DigitalObject/interfaces/IDigitalObjectService.java
+++ src/main/java/org/zim/gamsapi/DigitalObject/interfaces/IDigitalObjectService.java
@@ -7,6 +7,7 @@ import org.zim.gamsapi.DigitalObject.exceptions.DigitalObjectNotFoundException;
 import org.zim.gamsapi.Project.Project;
 
 import java.util.List;
+import java.util.Set;
 
 public interface IDigitalObjectService {
 
@@ -25,5 +26,13 @@ public interface IDigitalObjectService {
   void delete(DigitalObject digitalObject);
 
   void deleteAllForProject(Project project);
+
+  /**
+   * Allows to assign child objects to a parent object.
+   * @param parentObject the parent object
+   * @param childObjects the child objects
+   * @return the parent object with the assigned child objects
+   */
+  DigitalObject assignChildObjects(DigitalObject parentObject, Set<DigitalObject> childObjects);
 
 }
