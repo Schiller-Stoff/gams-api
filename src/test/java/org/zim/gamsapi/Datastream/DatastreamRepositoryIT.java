1924231cee93711d3adf670072acd95cd651b824
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2024-03-20 12:48:07 +0100

    added basic saving and deletion repository layer tests for datastreams
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/102

---
+++ src/test/java/org/zim/gamsapi/Datastream/DatastreamRepositoryIT.java
@@ -0,0 +1,56 @@
+package org.zim.gamsapi.Datastream;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.zim.gamsapi.IntegrationTest;
+
+/**
+ * Integration test for the DatastreamRepository.
+ *
+ */
+public class DatastreamRepositoryIT extends IntegrationTest {
+
+    @Autowired
+    IDatastreamRepository datastreamRepository;
+
+
+    /**
+     * Tests if a saved datastream exists with the expected globalID.
+     */
+    @Test
+    public void saveDatastreamExistsWithExpectedID() {
+        Datastream datastream = datastreamRepository.save(Datastream.builder().build());
+        Assertions.assertThat(
+                datastreamRepository.findById(datastream.getGlobalId()))
+                .isNotNull()
+                .isPresent()
+                .get()
+                .extracting(Datastream::getGlobalId)
+                .isEqualTo(datastream.getGlobalId()
+        );
+        // clean up and check if successfully deleted
+        datastreamRepository.delete(datastream);
+        Assertions.assertThat(
+                datastreamRepository.findById(datastream.getGlobalId()))
+                .isNotNull()
+                .isNotPresent();
+    }
+
+    /**
+     * Tests if the datastream with the expected globalID was deleted.
+     */
+    @Test
+    public void deleteDatastreamRemovesDatastream() {
+        Datastream datastream = datastreamRepository.save(Datastream.builder().build());
+        datastreamRepository.delete(datastream);
+        Assertions.assertThat(
+                datastreamRepository.findById(datastream.getGlobalId()))
+                .isNotNull()
+                .isNotPresent();
+    }
+
+
+
+
+}
