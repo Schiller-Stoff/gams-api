f41af9c68afeed164c19c208a9af230677b8939d
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2024-03-20 14:46:30 +0100

    added more repo tests for datastreams
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/102

--- src/test/java/org/zim/gamsapi/Datastream/DatastreamRepositoryIT.java
+++ src/test/java/org/zim/gamsapi/Datastream/DatastreamRepositoryIT.java
@@ -1,9 +1,17 @@
 package org.zim.gamsapi.Datastream;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.zim.gamsapi.DigitalObject.DigitalObject;
+import org.zim.gamsapi.DigitalObject.IDigitalObjectRepository;
 import org.zim.gamsapi.IntegrationTest;
+import org.zim.gamsapi.Project.Project;
+import org.zim.gamsapi.Project.interfaces.IProjectRepository;
+import org.zim.gamsapi.enums.TestDatastream;
+import org.zim.gamsapi.enums.TestDigitalObject;
+import org.zim.gamsapi.enums.TestProject;
 
 /**
  * Integration test for the DatastreamRepository.
@@ -13,6 +21,22 @@ public class DatastreamRepositoryIT extends IntegrationTest {
 
     @Autowired
     IDatastreamRepository datastreamRepository;
+
+    @Autowired
+    IDigitalObjectRepository digitalObjectRepository;
+
+    @Autowired
+    IProjectRepository projectRepository;
+
+    @AfterEach
+    public void tearDown(){
+        // verify that the test data is being deleted after each test
+        projectRepository.delete(Project.builder().projectAbbr(TestProject.PROJECT_ABBR.getValue()).build());
+        Assertions.assertThat(
+                projectRepository.findById(TestProject.PROJECT_ABBR.getValue()))
+                .isNotNull()
+                .isNotPresent();
+    }
 
 
     /**
@@ -62,6 +86,77 @@ public class DatastreamRepositoryIT extends IntegrationTest {
                 .isNotPresent();
     }
 
+
+    /**
+     * Verifies that a datastream is empty optional if it does not exist.
+     */
+    @Test
+    public void findByDigitalObjectAndDsidReturnsEmptyOptionalIfDatastreamDoesNotExist() {
+        // first create and save test project
+        Project project = Project.builder().projectAbbr(TestProject.PROJECT_ABBR.getValue()).build();
+        projectRepository.save(project);
+        // then create and save testobject
+        DigitalObject objectToBeFound = DigitalObject.builder().id("1").project(project).build();
+        digitalObjectRepository.save(objectToBeFound);
+
+        Assertions.assertThat(
+                datastreamRepository.findByDigitalObjectAndDsid(objectToBeFound, "NOT_THERE"))
+                .isNotNull()
+                .isNotPresent();
+
+        // cleanup
+        digitalObjectRepository.delete(objectToBeFound);
+        // verify that the object was deleted
+        Assertions.assertThat(
+                digitalObjectRepository.findById(objectToBeFound.getId()))
+                .isNotNull()
+                .isNotPresent();
+
+        projectRepository.delete(project);
+        // verify that the project was deleted
+        Assertions.assertThat(
+                projectRepository.findById(project.getProjectAbbr()))
+                .isNotNull()
+                .isNotPresent();
+    }
+
+    /**
+     * Verifies that a datastream is returned if it exists.
+     */
+    @Test
+    public void findByDigitalAndDsidReturnsDatastreamIfDatastreamExists() {
+        // first create and save test project
+        Project project = Project.builder().projectAbbr(TestProject.PROJECT_ABBR.getValue()).build();
+        projectRepository.save(project);
+
+        DigitalObject digitalObject = DigitalObject.builder().id(TestDigitalObject.DIGITAL_OBJECT_ID.getValue()).project(project).build();
+        digitalObject = digitalObjectRepository.save(digitalObject);
+
+        Datastream datastream = Datastream.builder().digitalObject(digitalObject).dsid(TestDatastream.DATASTREAM_NAME.getValue()).build();
+        datastreamRepository.save(datastream);
+        Assertions.assertThat(
+                datastreamRepository.findByDigitalObjectAndDsid(digitalObject, datastream.getDsid()))
+                .isNotNull()
+                .isPresent()
+                .get()
+                .isEqualTo(datastream);
+
+        // cleanup
+        digitalObjectRepository.delete(digitalObject);
+        // verify that the object was deleted
+        Assertions.assertThat(digitalObjectRepository.findById(digitalObject.getId()))
+                .isNotNull()
+                .isNotPresent();
+        // project cleanup
+        projectRepository.delete(project);
+        // verify that the project was deleted
+        Assertions.assertThat(
+                projectRepository.findById(project.getProjectAbbr()))
+                .isNotNull()
+                .isNotPresent();
+
+
+    }
 
 
 
