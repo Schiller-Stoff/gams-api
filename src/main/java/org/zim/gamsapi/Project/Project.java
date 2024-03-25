ce40600dd58c6a1c85983cba5fee7d7d01f88850
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2024-03-25 10:26:12 +0100

    added convenience methods to establish / remove bidirectional relationships between project and digital object
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/105

--- src/main/java/org/zim/gamsapi/Project/Project.java
+++ src/main/java/org/zim/gamsapi/Project/Project.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
+import lombok.extern.slf4j.Slf4j;
 import org.zim.gamsapi.DigitalObject.DigitalObject;
 import org.zim.gamsapi.User.User;
 import java.util.HashSet;
@@ -21,6 +22,7 @@ import java.util.Set;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Slf4j
 public class Project {
 
   @NotBlank
@@ -42,6 +44,48 @@ public class Project {
   // manages bidirectional reference in json https://www.baeldung.com/jackson-bidirectional-relationships-and-infinite-recursion
   private Set<User> users;
 
+  /**
+   * Adds a digital object to the project.
+   * @param digitalObject the digital object to add
+   */
+  public void addDigitalObject(DigitalObject digitalObject) {
+    if(digitalObject == null){
+      String msg = String.format("Cannot assign a digital object with value null to the project %s", this);
+      log.error(msg);
+      throw new NullPointerException(msg);
+    }
+
+    if(digitalObject.getProject() != null){
+      String msg = String.format("Digital object %s is already assigned to a project.", digitalObject);
+      log.error(msg);
+      throw new IllegalArgumentException(msg);
+    }
+
+    digitalObjects.add(digitalObject);
+    digitalObject.setProject(this);
+  }
+
+  /**
+   * Removes a digital object from the project.
+   * @param digitalObject the digital object to remove
+   */
+  public void removeDigitalObject(DigitalObject digitalObject) {
+    if(digitalObject == null){
+      String msg = String.format("Cannot remove a digital object with value null from the project %s", this);
+      log.error(msg);
+      throw new NullPointerException(msg);
+    }
+
+    if(digitalObject.getProject() == null){
+      String msg = String.format("Digital object %s is not assigned to a project.", digitalObject);
+      log.error(msg);
+      throw new IllegalArgumentException(msg);
+    }
+
+    digitalObjects.remove(digitalObject);
+    digitalObject.setProject(null);
+  }
+
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
@@ -53,5 +97,9 @@ public class Project {
   @Override
   public int hashCode() {
     return Objects.hash(projectAbbr);
+  }
+
+  void setDigitalObjects(Set<DigitalObject> digitalObjects) {
+    this.digitalObjects = digitalObjects;
   }
 }
