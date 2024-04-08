542db45ca8939df8607e3f6f2ec8c5af9d458f70
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2024-04-08 11:45:13 +0200

    added a test against datastream service that should work but doesn't
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/106

--- src/test/java/org/zim/gamsapi/Datastream/DatastreamServiceIT.java
+++ src/test/java/org/zim/gamsapi/Datastream/DatastreamServiceIT.java
@@ -1,11 +1,10 @@
 package org.zim.gamsapi.Datastream;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.zim.gamsapi.Datastream.interfaces.IDatastreamService;
 import org.zim.gamsapi.DigitalObject.DigitalObject;
+import org.zim.gamsapi.DigitalObject.IDigitalObjectRepository;
 import org.zim.gamsapi.DigitalObject.exceptions.DigitalObjectNotFoundException;
 import org.zim.gamsapi.IntegrationTest;
 import org.zim.gamsapi.System.utils.DigitalObjectBuilder;
@@ -13,10 +12,27 @@ import org.zim.gamsapi.enums.TestDatastream;
 import org.zim.gamsapi.enums.TestDigitalObject;
 import org.zim.gamsapi.enums.TestProject;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class DatastreamServiceIT extends IntegrationTest {
 
   @Autowired
   IDatastreamService datastreamService;
+  @Autowired
+  IDigitalObjectRepository digitalObjectRepository;
+
+  @BeforeAll
+  public void setup(){
+    DigitalObject testObject = new DigitalObjectBuilder(TestDigitalObject.DIGITAL_OBJECT_ID.getValue())
+        .addProject(TestProject.PROJECT_ABBR.getValue())
+        .add()
+        .build();
+    digitalObjectRepository.save(testObject);
+  }
+
+  @AfterAll
+  public void tearDown(){
+    digitalObjectRepository.deleteAll();
+  }
 
   @Nested
   public class SaveDatastream {
@@ -24,7 +40,9 @@ public class DatastreamServiceIT extends IntegrationTest {
     @Test
     public void throwsIfReferencedDigitalObjectNotFound(){
 
-      DigitalObject digitalObject = new DigitalObjectBuilder(TestDigitalObject.DIGITAL_OBJECT_ID.getValue())
+      final String RANDOM_PID = "SOME_RANDOM_PID";
+
+      DigitalObject digitalObject = new DigitalObjectBuilder(RANDOM_PID)
           .addDatastream(TestDatastream.DSID.getValue())
           .add()
           .addProject(TestProject.PROJECT_ABBR.getValue())
@@ -39,6 +57,28 @@ public class DatastreamServiceIT extends IntegrationTest {
           () -> datastreamService.save(datastream)
       );
 
+      digitalObjectRepository.delete(digitalObject);
+    }
+
+    @Test
+    public void datastreamExistsAfterSaving(){
+
+      final String RANDOM_DSID = "SOME_RANDOM_DSID";
+      Datastream datastream = Datastream.builder()
+          .dsid(RANDOM_DSID)
+          .digitalObject(
+              new DigitalObjectBuilder(TestDigitalObject.DIGITAL_OBJECT_ID.getValue())
+                  .addProject(TestProject.PROJECT_ABBR.getValue())
+                  .add()
+                  .build()
+          )
+          .build();
+
+      datastreamService.save(datastream);
+
+      org.assertj.core.api.Assertions.assertThat(
+          datastreamService.findByDsid(TestDigitalObject.DIGITAL_OBJECT_ID.getValue(), RANDOM_DSID)
+      ).isNotNull();
 
     }
 
