b4eb431a641f3845c156467294d9f87f72b50415
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2024-04-18 11:50:24 +0200

    fixed digital object repository integration tests
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/107

--- src/test/java/org/zim/gamsapi/DigitalObject/IDigitalObjectRepositoryIT.java
+++ src/test/java/org/zim/gamsapi/DigitalObject/IDigitalObjectRepositoryIT.java
@@ -1,11 +1,15 @@
 package org.zim.gamsapi.DigitalObject;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.zim.gamsapi.IntegrationTest;
 import org.zim.gamsapi.Project.Project;
+import org.zim.gamsapi.Project.interfaces.IProjectRepository;
 import org.zim.gamsapi.System.configproperties.GAMSAPIProperties;
+import org.zim.gamsapi.enums.TestProject;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -13,6 +17,7 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class IDigitalObjectRepositoryIT extends IntegrationTest {
 
     private final static String PID = "testPid";
@@ -21,59 +26,139 @@ class IDigitalObjectRepositoryIT extends IntegrationTest {
     @Autowired
     IDigitalObjectRepository repository;
 
+    @Autowired
+    IProjectRepository projectRepository;
 
-    @BeforeEach
-    public void saveTestObject() {
-        repository.save(
+    Project testProject;
+
+    @BeforeAll
+    public void setup(){
+
+        testProject = Project
+            .builder()
+            .projectAbbr(TestProject.PROJECT_ABBR.getValue())
+            .build();
+
+        projectRepository.save(testProject);
+
+
+    }
+
+
+    @AfterAll
+    public void tearDown(){
+
+        repository.deleteAll();
+        projectRepository.deleteAll();
+
+        Assertions.assertThat(repository.findAll())
+            .isNotNull()
+            .isEmpty();
+
+        Assertions.assertThat(projectRepository.findAll())
+            .isNotNull()
+            .isEmpty();
+
+    }
+
+
+    @Nested
+    public class Save {
+
+        @Test
+        public void successfullySavesObject(){
+
+            final String RANDOM_PID = "RANDOM_PID";
+
+            repository.save(
                 new DigitalObjectBuilder()
-                    .id(PID)
-                    .project(Project.builder().projectAbbr(GAMSAPIProperties.DEMO_PROJECT_ABBR.name).build())
+                    .id(RANDOM_PID)
+                    .project(testProject)
                     .build()
-        );
+            );
+
+            Assertions.assertThat(repository.findById(RANDOM_PID))
+                .isNotNull()
+                .isPresent();
+
+            // cleanup
+            repository.deleteById(RANDOM_PID);
+
+
+        }
+
+
     }
 
     @Test
-    @Order(1)
     public void testFindByPid() {
 
-        Optional<DigitalObject> digitalObject = repository.findById(PID);
+        final String RANDOM_PID = "RANDOM_PID";
 
-        assertThat(digitalObject)
-                .isNotNull()
-                .isPresent()
-                .get()
-                .extracting(DigitalObject::getId)
-                .isEqualTo(PID);
+        repository.save(
+            new DigitalObjectBuilder()
+                .id(RANDOM_PID)
+                .project(testProject)
+                .build()
+        );
 
-        assertThat(digitalObject.get().getId())
-                .isNotNull()
-                .isEqualTo(PID);
+        repository.findById(RANDOM_PID);
+
+        Assertions.assertThat(repository.findById(RANDOM_PID))
+            .isNotNull()
+            .isPresent();
+
+        // cleanup
+        repository.deleteById(RANDOM_PID);
+
     }
 
     @Test
     @Order(2)
     public void testFindByProjectAbbr() {
 
-        List<DigitalObject> digitalObjects = repository.findDigitalObjectsByProject_ProjectAbbr(GAMSAPIProperties.DEMO_PROJECT_ABBR.name);
+        final String RANDOM_PID = "RANDOM_PID";
 
-        assertThat(digitalObjects)
-                .isNotNull()
-                .isExactlyInstanceOf(ArrayList.class)
-                .first()
-                .extracting(DigitalObject::getId)
-                .isEqualTo(PID);
+        repository.save(
+            new DigitalObjectBuilder()
+                .id(RANDOM_PID)
+                .project(testProject)
+                .build()
+        );
+
+        List<DigitalObject> digitalObjects = repository.findDigitalObjectsByProject_ProjectAbbr(testProject.getProjectAbbr());
+
+        Assertions.assertThat(digitalObjects)
+            .isNotNull()
+            .isExactlyInstanceOf(ArrayList.class)
+            .first()
+            .extracting(DigitalObject::getId)
+            .isEqualTo(RANDOM_PID);
+
+        // cleanup
+        repository.deleteById(RANDOM_PID);
     }
 
     @Test
-    @Order(3)
     @Transactional
     public void testDeleteAllByProjectAbbr() {
 
-        repository.deleteAllByProject_ProjectAbbr(GAMSAPIProperties.DEMO_PROJECT_ABBR.name);
+        final String RANDOM_PID = "RANDOM_PID";
 
-        Optional<DigitalObject> digitalObject = repository.findById(PID);
+        repository.save(
+            new DigitalObjectBuilder()
+                .id(RANDOM_PID)
+                .project(testProject)
+                .build()
+        );
 
-        assertThat(digitalObject).isEmpty();
+        repository.deleteAllByProject_ProjectAbbr(testProject.getProjectAbbr());
+
+        List<DigitalObject> digitalObjects = repository.findDigitalObjectsByProject_ProjectAbbr(testProject.getProjectAbbr());
+
+        Assertions.assertThat(digitalObjects)
+            .isNotNull()
+            .isEmpty();
 
     }
 }
