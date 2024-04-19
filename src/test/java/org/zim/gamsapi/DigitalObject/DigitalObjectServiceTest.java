edd8500cd3c0f36001de37e57868e7113762925b
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2024-04-19 12:05:45 +0200

    added test against saving a digital object via the service layer
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/107

---
+++ src/test/java/org/zim/gamsapi/DigitalObject/DigitalObjectServiceTest.java
@@ -0,0 +1,89 @@
+package org.zim.gamsapi.DigitalObject;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.zim.gamsapi.DigitalObject.interfaces.IDigitalObjectService;
+import org.zim.gamsapi.IntegrationTest;
+import org.zim.gamsapi.Project.Project;
+import org.zim.gamsapi.Project.interfaces.IProjectRepository;
+import org.zim.gamsapi.enums.TestProject;
+
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class DigitalObjectServiceTest extends IntegrationTest {
+
+  @Autowired
+  IProjectRepository projectRepository;
+
+  @Autowired
+  IDigitalObjectRepository digitalObjectRepository;
+
+  @Autowired
+  IDigitalObjectService digitalObjectService;
+
+  Project testProject;
+
+  @BeforeAll
+  public void setup(){
+
+    testProject = Project
+      .builder()
+      .projectAbbr(TestProject.PROJECT_ABBR.getValue())
+      .build();
+
+    projectRepository.save(testProject);
+
+  }
+
+  @AfterAll
+  public void tearDown(){
+
+    projectRepository.deleteAll();
+    digitalObjectRepository.deleteAll();
+
+    Assertions.assertThat(digitalObjectRepository.findAll())
+      .isNotNull()
+      .isEmpty();
+
+    Assertions.assertThat(projectRepository.findAll())
+        .isNotNull()
+        .isEmpty();
+  }
+
+  @Nested
+  public class Save {
+
+
+    @Test
+    public void successFullySavesSimpleDigitalObject() {
+      // given
+
+      DigitalObject digitalObject = new DigitalObjectBuilder()
+        .id("testPid")
+        .project(testProject)
+        .build();
+
+      // when
+      DigitalObject savedDigitalObject = digitalObjectService.save(digitalObject);
+
+      // then
+      Assertions.assertThat(savedDigitalObject).isNotNull();
+      Assertions.assertThat(savedDigitalObject.getId()).isNotNull();
+      Assertions.assertThat(savedDigitalObject.getProject()).isEqualTo(testProject);
+      // parent was not set
+      Assertions.assertThat(savedDigitalObject.getParent()).isNull();
+      // considered equal because of same id
+      Assertions.assertThat(savedDigitalObject).isEqualTo(digitalObject);
+
+      // cleanup
+      digitalObjectRepository.delete(digitalObject);
+    }
+
+
+
+
+
+  }
+
+}
