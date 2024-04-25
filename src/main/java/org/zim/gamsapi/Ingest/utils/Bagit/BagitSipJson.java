f8e0ed1d86afcc3c39677648cf33b64825458dad
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2024-04-25 14:56:52 +0200

    added project abbreviation as required from sip.json
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/115

--- src/main/java/org/zim/gamsapi/Ingest/utils/Bagit/BagitSipJson.java
+++ src/main/java/org/zim/gamsapi/Ingest/utils/Bagit/BagitSipJson.java
@@ -2,13 +2,16 @@ package org.zim.gamsapi.Ingest.utils.Bagit;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Container for the bagit sip.json file defined by invenio / CERN.
+ * TODO improve validation e.g add @NotEmpty etc.
  */
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -17,6 +20,14 @@ public class BagitSipJson {
     @JsonProperty("recid")
     @NotNull
     private String id;
+
+    /**
+     * Abbreviation of the GAMS project.
+     * TODO missing in test data atm?
+     */
+    @NotEmpty
+    @Size(min = 1, max = 10)
+    private String project;
 
     @NotNull
     private String title;
