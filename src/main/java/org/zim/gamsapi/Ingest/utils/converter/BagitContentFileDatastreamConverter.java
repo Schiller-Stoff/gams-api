46c0b7c138839efa29c4c0058c7e115fba1f3235
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2024-04-29 11:01:18 +0200

    added converter for bagitContentFile to datastream
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/115

---
+++ src/main/java/org/zim/gamsapi/Ingest/utils/converter/BagitContentFileDatastreamConverter.java
@@ -0,0 +1,38 @@
+package org.zim.gamsapi.Ingest.utils.converter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.convert.converter.Converter;
+import org.zim.gamsapi.Datastream.Datastream;
+import org.zim.gamsapi.Datastream.DatastreamBuilder;
+import org.zim.gamsapi.Ingest.utils.Bagit.BagitContentFile;
+import org.zim.gamsapi.MetadataBaseEntityBuilder;
+
+
+/**
+ * Converts a BagitContentFile to a Datastream.
+ * Cannot set all properties, like reading the content from the file or filename etc for a datastream instance
+ */
+@Slf4j
+public class BagitContentFileDatastreamConverter implements Converter<BagitContentFile, Datastream>{
+
+
+  @Override
+  public Datastream convert(BagitContentFile source) {
+
+    // TODO test
+
+    return new DatastreamBuilder()
+        .dsid(source.getDsid())
+        .mimeType(source.getMimetype())
+        .size(source.getSize())
+        .baseMetadata(new MetadataBaseEntityBuilder()
+            .title(source.getTitle())
+            .creator(source.getCreator())
+            .description(source.getDescription())
+            .publisher(source.getPublisher())
+            .rights(source.getRights())
+            .build())
+        .build();
+
+  }
+}
