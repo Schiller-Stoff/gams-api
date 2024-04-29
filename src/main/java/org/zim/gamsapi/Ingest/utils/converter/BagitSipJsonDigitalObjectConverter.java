b938f310cda2b2fd1efd6fdb0ddc6ab58be8bf23
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2024-04-29 10:49:59 +0200

    added a converter class from BagitSipJson -> Digital object
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/115

---
+++ src/main/java/org/zim/gamsapi/Ingest/utils/converter/BagitSipJsonDigitalObjectConverter.java
@@ -0,0 +1,36 @@
+package org.zim.gamsapi.Ingest.utils.converter;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import org.zim.gamsapi.DigitalObject.DigitalObject;
+import org.zim.gamsapi.DigitalObject.DigitalObjectBuilder;
+import org.zim.gamsapi.Ingest.utils.Bagit.BagitSipJson;
+import org.zim.gamsapi.MetadataBaseEntityBuilder;
+
+/**
+ * Converts a BagitSipJson to a DigitalObject.
+ *
+ */
+@Component
+public class BagitSipJsonDigitalObjectConverter implements Converter<BagitSipJson, DigitalObject> {
+
+  // TODO needs tests!
+
+    @Override
+    public DigitalObject convert(BagitSipJson source) {
+        return new DigitalObjectBuilder()
+            .id(source.getId())
+            .project(source.getProject())
+            .objectType(source.getObjectType())
+            .parent(new DigitalObjectBuilder().project(source.getProject()).id(source.getParent()).build())
+            .types(source.getTypes())
+            .baseMetadata(new MetadataBaseEntityBuilder()
+                .title(source.getTitle())
+                .creator(source.getCreator())
+                .description(source.getDescription())
+                .publisher(source.getPublisher())
+                .rights(source.getRights())
+                .build())
+            .build();
+    }
+}
