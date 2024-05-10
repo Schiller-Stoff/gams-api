8b9687b706412373e6eae185f1779574a333cd5a
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2024-05-10 11:06:29 +0200

    added role definition of project admin / project editor
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/101

--- src/main/java/org/zim/gamsapi/System/security/GAMSAPISecurityRoles.java
+++ src/main/java/org/zim/gamsapi/System/security/GAMSAPISecurityRoles.java
@@ -5,13 +5,56 @@ package org.zim.gamsapi.System.security;
  */
 public enum GAMSAPISecurityRoles {
 
+  //  TODO remove public access to properties -> let things be built via getters.
+
   ADMINISTRATOR("ROLE_administrator"),
-  EDITOR("ROLE_editor");
+
+  // TODO remove this role?
+  EDITOR("ROLE_editor"),
+
+  PROJECT_ADMINISTRATOR("admin"),
+
+  PROJECT_EDITOR("editor"),
+
+  PROJECT_VIEWER("viewer"),
+
+  ROLE_DELIMITER("_");
 
   public final String name;
 
   GAMSAPISecurityRoles(String name){
     this.name = name;
+  }
+
+
+  /**
+   * Returns the full project admin role
+   * TODO test
+   * @param projectAbbr the project abbreviation
+   * @return the full project admin role
+   */
+  public static String getProjectAdmin(String projectAbbr) {
+    return "ROLE_" + projectAbbr + GAMSAPISecurityRoles.ROLE_DELIMITER.name + GAMSAPISecurityRoles.PROJECT_ADMINISTRATOR.name;
+  }
+
+  /**
+   * TODO test
+   * Returns the full project editor role
+   * @param projectAbbr the project abbreviation
+   * @return the full project editor role
+   */
+  public static String getProjectEditor(String projectAbbr) {
+    return "ROLE_" + projectAbbr + GAMSAPISecurityRoles.ROLE_DELIMITER.name + GAMSAPISecurityRoles.PROJECT_EDITOR.name;
+  }
+
+  /**
+   * TODO test
+   * Returns the full project viewer role
+   * @param projectAbbr the project abbreviation
+   * @return the full project viewer role
+   */
+  public static String getProjectViewer(String projectAbbr) {
+    return "ROLE_" + projectAbbr + GAMSAPISecurityRoles.ROLE_DELIMITER.name + GAMSAPISecurityRoles.PROJECT_VIEWER.name;
   }
 
 }
