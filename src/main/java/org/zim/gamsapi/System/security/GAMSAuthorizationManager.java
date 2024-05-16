d78fb00d461af0bbd3c857fffcace1195be44adc
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2024-05-16 12:24:30 +0200

    removed unneeded import
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/101

--- src/main/java/org/zim/gamsapi/System/security/GAMSAuthorizationManager.java
+++ src/main/java/org/zim/gamsapi/System/security/GAMSAuthorizationManager.java
@@ -9,7 +9,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.zim.gamsapi.System.security.exceptions.AuthorizationConfigurationException;
 import org.zim.gamsapi.System.security.exceptions.UserAuthenticationRequiredException;
 import org.zim.gamsapi.System.security.exceptions.UserNotAssignedToProjectException;
