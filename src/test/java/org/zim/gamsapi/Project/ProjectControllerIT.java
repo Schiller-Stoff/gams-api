9308dd26c19a013271cfcc7da587e344627cf366
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2024-05-16 14:32:37 +0200

    reenabled security here (because: get requests are not protected!)
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/101

--- src/test/java/org/zim/gamsapi/Project/ProjectControllerIT.java
+++ src/test/java/org/zim/gamsapi/Project/ProjectControllerIT.java
@@ -12,7 +12,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.zim.gamsapi.IntegrationTest;
 import org.zim.gamsapi.Project.interfaces.IProjectRepository;
 
-@AutoConfigureMockMvc(addFilters = false) // disable security for class
+@AutoConfigureMockMvc
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ProjectControllerIT extends IntegrationTest {
 
