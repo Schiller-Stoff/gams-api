281b15d2f9493a40af540cecc296a5a04eea3ebb
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2024-05-22 09:17:22 +0200

    implemented first working authorization test using the mocking of spring security
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/101

--- src/test/java/org/zim/gamsapi/System/security/AuthorizationIT.java
+++ src/test/java/org/zim/gamsapi/System/security/AuthorizationIT.java
@@ -6,20 +6,15 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.mock.web.MockPart;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+import org.zim.gamsapi.Ingest.utils.IngestStatics;
 import org.zim.gamsapi.IntegrationTest;
-import org.zim.gamsapi.System.configproperties.GAMSAPIProperties;
 import org.zim.gamsapi.System.security.exceptions.UserNotAssignedToProjectException;
-
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import org.zim.gamsapi.enums.TestProject;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Tests Authorization settings in the application
@@ -31,16 +26,26 @@ public class AuthorizationIT extends IntegrationTest {
   @Autowired
   private MockMvc mockMvc;
 
+
   @Test
-  @Disabled
-  public void ingestRequiresAuthorization() throws Exception {
+  public void demoUserNotAuthorizedForProjectIngest_throwsUserNotAssignedToProjectException() {
 
-    // currently disabled because --> need to think about
+    byte[] zippedBag = new byte[0];
+    MockPart mockPart = new MockPart(IngestStatics.FORM_PART_NAME.name, "test.zip", zippedBag);
 
-    final String INGEST_ENDPOINT =  "/api/v1/projects/" + GAMSAPIProperties.DEMO_PROJECT_ABBR.name + "/objects/test";
-    mockMvc.perform(MockMvcRequestBuilders.post(INGEST_ENDPOINT).content(new byte[0])
-            .with(user(GAMSAPIProperties.ADMIN_USER_NAME.name).roles(GAMSAPIProperties.DEMO_PROJECT_ABBR.name)))
-            .andExpect(MockMvcResultMatchers.status().isOk());
+    Assertions.assertThrows(UserNotAssignedToProjectException.class, () -> {
+      mockMvc
+          .perform(
+              multipart("/api/v1/projects/{projectAbbr}/objects", TestProject.PROJECT_ABBR.getValue())
+                  .part(mockPart)
+                  .with(SecurityMockMvcRequestPostProcessors
+                      .user("UNKNOWN_USER")
+                      .roles("UNKNOWN_ROLE")
+                  )
+          )
+          .andExpect(status().is4xxClientError());
+    });
+
   }
 
 }
