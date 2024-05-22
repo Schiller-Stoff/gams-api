1e852cd4f199f4dc81e4347692cf03b2928e608c
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2024-05-22 10:13:38 +0200

    added test that proves that a server error is thrown if auth was successfull but the zip is malformed
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/101

--- src/test/java/org/zim/gamsapi/System/security/AuthorizationIT.java
+++ src/test/java/org/zim/gamsapi/System/security/AuthorizationIT.java
@@ -1,8 +1,6 @@
 package org.zim.gamsapi.System.security;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -47,5 +45,36 @@ public class AuthorizationIT extends IntegrationTest {
     });
 
   }
+
+  @Test
+  public void projectAdminAuthorizedForProjectIngest_throwsServerErrorAtProcessingAfterBeingAuthorized() throws Exception {
+
+    byte[] zippedBag = new byte[0];
+    MockPart mockPart = new MockPart(IngestStatics.FORM_PART_NAME.name, "test.zip", zippedBag);
+
+    String testProjectAdminRole = GAMSAPISecurityRoles.getProjectAdmin(TestProject.PROJECT_ABBR.getValue());
+    // mock method needs role prefix excluded.
+    testProjectAdminRole = testProjectAdminRole.replace(GAMSAPISecurityRoles.ROLE_PREFIX.name, "");
+
+    mockMvc
+        .perform(
+            multipart("/api/v1/projects/{projectAbbr}/objects", TestProject.PROJECT_ABBR.getValue())
+                .part(mockPart)
+                .with(SecurityMockMvcRequestPostProcessors
+                    .user("SOME_USER")
+                    .roles(testProjectAdminRole)
+                )
+        )
+        .andExpect(
+            status().is5xxServerError()
+        );
+
+  }
+
+  // TODO test: ingest denied if only project-viewer?
+
+  // TODO ingest allowed for global admin?
+
+  // TODO add more tests? ...
 
 }
