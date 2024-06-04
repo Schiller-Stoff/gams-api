43997ec14e883e19358a77feafcea360d2acdd7b
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2024-06-04 10:24:40 +0200

    removed oudtated entity functionality
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/120

--- src/main/java/org/zim/gamsapi/User/User.java
+++ src/main/java/org/zim/gamsapi/User/User.java
@@ -1,12 +1,6 @@
 package org.zim.gamsapi.User;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.*;
-import org.zim.gamsapi.Project.Project;
-import java.util.Set;
 
 /**
  * Model representing an user in terms of user management.
@@ -22,15 +16,11 @@ public class User {
   /**
    * The user id.
    */
-  @Id
-  @NotNull
   private String userid;
 
   /**
    * The user name.
    */
-  @Column(nullable = false, unique = true)
-  @NotBlank
   private String username;
 
 }
