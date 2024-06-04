e1afbc1f1ccaae37019f274e8680f6460bf6c77d
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2024-06-04 10:23:21 +0200

    removed operations based on relationship to the removed user entity
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/120

--- src/main/java/org/zim/gamsapi/Project/ProjectService.java
+++ src/main/java/org/zim/gamsapi/Project/ProjectService.java
@@ -9,11 +9,7 @@ import org.zim.gamsapi.Project.exceptions.ProjectNotFoundException;
 import org.zim.gamsapi.Project.interfaces.IProjectRepository;
 import org.zim.gamsapi.Project.interfaces.IProjectService;
 import org.zim.gamsapi.User.User;
-import org.zim.gamsapi.User.exceptions.UserNotFoundException;
-import org.zim.gamsapi.User.interfaces.IUserRepository;
-
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -21,7 +17,6 @@ import java.util.stream.Collectors;
 public class ProjectService implements IProjectService {
 
   private final IProjectRepository projectRepository;
-  private final IUserRepository userRepository;
 
   @Override
   @Transactional
@@ -34,25 +29,10 @@ public class ProjectService implements IProjectService {
       throw new ProjectAlreadyExistsException(msg);
     }
 
-    Optional<User> userOptional = userRepository.findByUsername(user.getUsername());
-    if(userOptional.isEmpty()){
-      String msg = String.format("Cannot find user with name %s. Aborting creation of project.", user.getUsername());
-      log.error(msg);
-      throw new UserNotFoundException(msg);
-    }
-
-    User userToSave = userOptional.get();
-    // add user to project and save project
-    project.setUsers(new HashSet<>(Set.of(user)));
     projectRepository.save(project);
-    log.debug("Create project - updated user associations {}", project);
+    log.trace("Saved project {}", project);
 
-    // add project to user and save user
-    userToSave.getProjects().add(project);
-    userToSave.setProjects(new HashSet<>(userToSave.getProjects()));
-    userToSave = userRepository.save(userToSave);
-    log.debug("Create project - updated project associations {}", userToSave);
-    return userToSave;
+    return user;
   }
 
 
@@ -66,24 +46,6 @@ public class ProjectService implements IProjectService {
     });
 
     log.trace("Found project {}", foundProject);
-
-    // update users referenced in project AND projects referenced in user
-    userRepository.findAll().forEach(user -> {
-      // remove project association from user
-      Set<Project> newProjects = user.getProjects().stream()
-              .filter(assignedProject -> !assignedProject.getProjectAbbr().equals(project.getProjectAbbr()))
-              .collect(Collectors.toSet());
-      user.setProjects(new HashSet<>(newProjects));
-      userRepository.save(user);
-
-      // remove user association from project
-      Set<User> newAssignedUsers =  foundProject.getUsers().stream()
-              .filter(assignedUser -> !assignedUser.getUsername().equals(user.getUsername()))
-              .collect(Collectors.toSet());
-      foundProject.setUsers(new HashSet<>(newAssignedUsers));
-      projectRepository.save(foundProject);
-    });
-
     projectRepository.delete(foundProject);
     log.info("Successfully deleted project {}", foundProject);
   }
