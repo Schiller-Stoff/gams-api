076ab2b51a95f5d65a3aaf45392a2aeb60b22ba1
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2024-06-05 10:48:02 +0200

    added tests to prove that only admins might create / delete projects.
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/121

--- src/test/java/org/zim/gamsapi/System/security/AuthorizationIT.java
+++ src/test/java/org/zim/gamsapi/System/security/AuthorizationIT.java
@@ -1,14 +1,20 @@
 package org.zim.gamsapi.System.security;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.mock.web.MockPart;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.zim.gamsapi.Ingest.utils.IngestStatics;
 import org.zim.gamsapi.IntegrationTest;
+import org.zim.gamsapi.Project.Project;
+import org.zim.gamsapi.Project.interfaces.IProjectRepository;
 import org.zim.gamsapi.System.security.exceptions.UserNotAssignedToProjectException;
 import org.zim.gamsapi.enums.TestProject;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
@@ -23,6 +29,9 @@ public class AuthorizationIT extends IntegrationTest {
 
   @Autowired
   private MockMvc mockMvc;
+
+  @Autowired
+  private IProjectRepository projectRepository;
 
 
   @Test
@@ -116,6 +125,60 @@ public class AuthorizationIT extends IntegrationTest {
 
   }
 
+  @Nested
+  public class ProjectAuthorization {
 
+    @Test
+    public void anonymousUserNotAuthorizedForProjectCreation_redirects() throws Exception {
+
+      final String TEST_PROJECT_ABBR = "FOO";
+      final String TEST_URL = "/api/v1/projects/" + TEST_PROJECT_ABBR;
+
+      mockMvc
+          .perform(
+              MockMvcRequestBuilders.post(TEST_URL)
+                  .with(SecurityMockMvcRequestPostProcessors.anonymous())
+          )
+          .andExpect(status().is3xxRedirection());
+    }
+
+    @Test
+    public void adminMayCreateAProject() throws Exception {
+
+      final String TEST_PROJECT_ABBR = TestProject.PROJECT_ABBR.getValue();
+      final String TEST_URL = "/api/v1/projects/" + TEST_PROJECT_ABBR;
+
+      mockMvc
+          .perform(
+              MockMvcRequestBuilders.put(TEST_URL)
+                  .with(
+                      SecurityMockMvcRequestPostProcessors.oidcLogin().authorities(new SimpleGrantedAuthority(GAMSAPIAuthorities.getAdmin()))
+                  )
+          ).andExpect(status().is2xxSuccessful());
+
+      org.assertj.core.api.Assertions.assertThat(projectRepository.findById(TEST_PROJECT_ABBR))
+          .isPresent();
+
+      // cleanup
+      projectRepository.deleteAll();
+
+    }
+
+    @Test
+    public void anonymousUserNotAuthorizedForProjectDeletion_redirects() throws Exception {
+
+      final String TEST_PROJECT_ABBR = "FOO";
+      final String TEST_URL = "/api/v1/projects/" + TEST_PROJECT_ABBR;
+
+      mockMvc
+          .perform(
+              MockMvcRequestBuilders.delete(TEST_URL)
+                  .with(SecurityMockMvcRequestPostProcessors.anonymous())
+          )
+          .andExpect(status().is3xxRedirection());
+    }
+
+
+  }
 
 }
