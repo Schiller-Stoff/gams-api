30f8e9e3fb191378ac19be34aaca0f28ceb34d41
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2024-06-06 14:01:05 +0200

    the redirect for the auth endpoint is now reverse-proxy aware (considers x-forwarded-host etc.)
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/123

--- src/main/java/org/zim/gamsapi/System/security/AuthEndpointController.java
+++ src/main/java/org/zim/gamsapi/System/security/AuthEndpointController.java
@@ -2,6 +2,10 @@ package org.zim.gamsapi.System.security;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.zim.gamsapi.System.utils.ControllerUtils;
+
+import java.util.Map;
 
 /**
  * Introduces the /auth endpoint that redirects to the oauth2 login endpoint.
@@ -13,9 +17,9 @@ public class AuthEndpointController {
    * Redirects to the oauth2 login endpoint.
    */
   @GetMapping("/api/v1/auth")
-  public String login() {
-    // TODO careful! - this redirect is not that simple (what if proxy before)?
-    return "redirect:/oauth2/authorization/gams-realm";
+  public String login(@RequestHeader Map<String, String> requestHeader) {
+    String origin = ControllerUtils.resolveProxiedOrigin(requestHeader);
+    return "redirect:" + origin + "oauth2/authorization/gams-realm";
   }
 
 
