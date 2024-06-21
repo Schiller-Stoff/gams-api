7eadbcc646535b4aefd7b3df5f4bb7316fd88b18
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2024-06-21 16:22:12 +0200

    organized import
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/128

--- src/main/java/org/zim/gamsapi/Integration/Common/exceptions/IntegrationDataProcessingException.java
+++ src/main/java/org/zim/gamsapi/Integration/Common/exceptions/IntegrationDataProcessingException.java
@@ -1,7 +1,6 @@
 package org.zim.gamsapi.Integration.Common.exceptions;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
 
 /**
  * Represents error states related to data processing, e.g. if a file could not be parsed.
