423e970a635a25752b2a94d5d688d193c4578050
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2024-09-04 11:10:12 +0200

    added errors related to file operations for datastreams
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/130

---
+++ src/main/java/org/zim/gamsapi/Datastream/exceptions/DatastreamCannotWriteFileException.java
@@ -0,0 +1,14 @@
+package org.zim.gamsapi.Datastream.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+/**
+ * Represents error state where a datastream's file cannot be written to a file (on the filesystem).
+ */
+public class DatastreamCannotWriteFileException extends DatastreamException {
+
+  public DatastreamCannotWriteFileException(String reason) {
+    super(HttpStatus.INTERNAL_SERVER_ERROR, reason);
+  }
+
+}
