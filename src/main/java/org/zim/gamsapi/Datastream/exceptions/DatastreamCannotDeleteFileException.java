77a628441832abf7e13816e56999c51c178c3001
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2024-09-10 13:41:57 +0200

    exception now extends transaction system exception
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/130

--- src/main/java/org/zim/gamsapi/Datastream/exceptions/DatastreamCannotDeleteFileException.java
+++ src/main/java/org/zim/gamsapi/Datastream/exceptions/DatastreamCannotDeleteFileException.java
@@ -1,14 +1,15 @@
 package org.zim.gamsapi.Datastream.exceptions;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.transaction.TransactionSystemException;
 
 /**
  * Represents error state where a datastream's file cannot be deleted from the filesystem.
  */
-public class DatastreamCannotDeleteFileException extends DatastreamException {
+public class DatastreamCannotDeleteFileException extends TransactionSystemException {
 
   public DatastreamCannotDeleteFileException(String reason) {
-    super(HttpStatus.INTERNAL_SERVER_ERROR, reason);
+    super(reason);
   }
 
 }
