65ff84b18cac7af0c10c847131ffd9edb60ad21c
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2024-09-13 12:35:01 +0200

    added methods that allow to easily generate a datastream for testing purposes
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/130

--- src/test/java/org/zim/gamsapi/enums/TestDatastream.java
+++ src/test/java/org/zim/gamsapi/enums/TestDatastream.java
@@ -1,7 +1,12 @@
 package org.zim.gamsapi.enums;
 
+import org.zim.gamsapi.Datastream.Datastream;
+import org.zim.gamsapi.Datastream.DatastreamBuilder;
+import org.zim.gamsapi.DigitalObject.DigitalObject;
+
 /**
  * Enum for test datastream.
+ * Provides helpers for the generations of test datastreams.
  */
 public enum TestDatastream {
 
@@ -18,5 +23,31 @@ public enum TestDatastream {
 
     public String getValue() {
         return value;
+    }
+
+    /**
+     * Generates a test datastream using the values defined in the enum.
+     * TODO test
+     * @param digitalObject The digital object to which the datastream belongs.
+     * @return The generated datastream.
+     */
+    public static Datastream generate(DigitalObject digitalObject){
+        return generate(digitalObject, DSID.getValue());
+    }
+
+    /**
+     * Generates a test datastream using the values defined in the enum.
+     * TODO test
+     * @param digitalObject The digital object to which the datastream belongs.
+     * @param dsid The dsid of the datastream.
+     * @return The generated datastream.
+     */
+    public static Datastream generate(DigitalObject digitalObject, String dsid){
+        return new DatastreamBuilder()
+            .dsid(dsid)
+            .digitalObject(digitalObject)
+            .baseMetadata(TestMetadataBaseEntity.generate())
+            .size((long) CONTENT.getValue().getBytes().length)
+            .build();
     }
 }
