f28ad142b8ee520afbda3b0ab3afd021a077749b
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2024-09-13 14:01:44 +0200

    added validation / contraint violation tests for the datastream entity
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/130

--- src/test/java/org/zim/gamsapi/Datastream/DatastreamTest.java
+++ src/test/java/org/zim/gamsapi/Datastream/DatastreamTest.java
@@ -1,16 +1,21 @@
 package org.zim.gamsapi.Datastream;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.zim.gamsapi.DigitalObject.DigitalObject;
 import org.zim.gamsapi.DigitalObject.DigitalObjectBuilder;
 import org.zim.gamsapi.MetadataBaseEntity;
-import org.zim.gamsapi.MetadataBaseEntityBuilder;
 import org.zim.gamsapi.UnitTest;
 import org.zim.gamsapi.enums.TestDatastream;
-import org.zim.gamsapi.enums.TestDigitalObject;
 import org.zim.gamsapi.enums.TestMetadataBaseEntity;
+
+import java.util.Set;
 
 public class DatastreamTest extends UnitTest {
 
@@ -88,6 +93,82 @@ public class DatastreamTest extends UnitTest {
           datastream::deriveDatastreamId
       );
     }
+
+  }
+
+  @Nested
+  public class ValidationTest {
+
+    private static ValidatorFactory validatorFactory;
+    private static Validator validator;
+
+    private final DigitalObject TEST_DIGITAL_OBJECT = new DigitalObjectBuilder()
+        .id("FOO_BAR")
+        .project("12345")
+        .baseMetadata(testMetadataBaseEntity)
+        .build();
+
+    @BeforeAll
+    static void init() {
+      validatorFactory = Validation.buildDefaultValidatorFactory();
+      validator = validatorFactory.getValidator();
+    }
+
+    @Test
+    public void testDatastreamShouldNotRaiseAnyConstraintViolation() {
+      Datastream datastream = TestDatastream.generate(
+          TEST_DIGITAL_OBJECT
+      );
+
+      Set<ConstraintViolation<Datastream>> violationSet = validator.validate(datastream);
+      org.assertj.core.api.Assertions.assertThat(violationSet).isEmpty();
+    }
+
+
+    @Test
+    public void testDatastreamShouldRaiseConstraintViolationIfDsidIsNull() {
+      Datastream datastream = TestDatastream.generate(
+          TEST_DIGITAL_OBJECT
+      );
+      datastream.setDsid(null);
+
+      Set<ConstraintViolation<Datastream>> violationSet = validator.validate(datastream);
+      org.assertj.core.api.Assertions.assertThat(violationSet.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void testDatastreamShouldRaiseConstraintViolationIfDigitalObjectIsNull() {
+      Datastream datastream = TestDatastream.generate(
+          TEST_DIGITAL_OBJECT
+      );
+      datastream.setDigitalObject(null);
+
+      Set<ConstraintViolation<Datastream>> violationSet = validator.validate(datastream);
+      org.assertj.core.api.Assertions.assertThat(violationSet.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void testDatastreamShouldRaiseConstraintViolationIfSizeIsNull() {
+      Datastream datastream = TestDatastream.generate(
+          TEST_DIGITAL_OBJECT
+      );
+      datastream.setSize(null);
+
+      Set<ConstraintViolation<Datastream>> violationSet = validator.validate(datastream);
+      org.assertj.core.api.Assertions.assertThat(violationSet.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void testDatastreamShouldRaiseConstraintViolationIfMimeTypeIsNull() {
+      Datastream datastream = TestDatastream.generate(
+          TEST_DIGITAL_OBJECT
+      );
+      datastream.setMimeType(null);
+
+      Set<ConstraintViolation<Datastream>> violationSet = validator.validate(datastream);
+      org.assertj.core.api.Assertions.assertThat(violationSet.size()).isEqualTo(1);
+    }
+
 
   }
 
