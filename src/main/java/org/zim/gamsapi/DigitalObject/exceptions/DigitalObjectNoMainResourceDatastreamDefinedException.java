e3e819fe5291dbb0596d1c7133ede3e7d19e81a5
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2025-04-03 09:03:29 +0200

    added exception for mainResource not defined
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/208

---
+++ src/main/java/org/zim/gamsapi/DigitalObject/exceptions/DigitalObjectNoMainResourceDatastreamDefinedException.java
@@ -0,0 +1,13 @@
+package org.zim.gamsapi.DigitalObject.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+/**
+ * Exception representing a digital object without a mainResource datastream defined
+ * but needed in given situation.
+ */
+public class DigitalObjectNoMainResourceDatastreamDefinedException extends DigitalObjectException {
+  public DigitalObjectNoMainResourceDatastreamDefinedException(String message) {
+    super(HttpStatus.INTERNAL_SERVER_ERROR, message);
+  }
+}
