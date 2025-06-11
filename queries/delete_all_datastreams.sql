9066203cdc557903638ddfb69980a9c1d1e15ccc
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2025-06-11 09:49:17 +0200

    removed outdated query file
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/214

--- queries/delete_all_datastreams.sql
+++
@@ -1,7 +0,0 @@
-DELETE FROM datastream
-WHERE digital_object_id IN (
-    SELECT dig_obj.id
-    FROM digital_object dig_obj
-             JOIN project cur_proj ON cur_proj.project_abbr = dig_obj.project_project_abbr
-    WHERE cur_proj.project_abbr = 'demo'
-)
