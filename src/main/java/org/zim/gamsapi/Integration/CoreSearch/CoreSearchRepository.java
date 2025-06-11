d4180954e002081698bc50bfb83f1077a8e30d6d
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2025-06-11 09:53:55 +0200

    temporarily removed elastic integration
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/214

--- src/main/java/org/zim/gamsapi/Integration/CoreSearch/CoreSearchRepository.java
+++
@@ -1,9 +0,0 @@
-package org.zim.gamsapi.Integration.CoreSearch;
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface CoreSearchRepository extends ElasticsearchRepository<CoreSearchEntity, String> {
-
-
-
-}
