75e312b34e89c8deeb49578c0b196a9fbc92b48e
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2025-06-13 12:22:17 +0200

    moved dublin core search logic to different class
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/215

--- src/main/java/org/zim/gamsapi/Project/ProjectController.java
+++ src/main/java/org/zim/gamsapi/Project/ProjectController.java
@@ -27,7 +27,6 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -202,94 +201,5 @@ public class ProjectController {
         .build();
   }
 
-  /**
-   * Search for digital objects based on Dublin Core metadata.
-   * @param projectAbbrs list of project abbreviations
-   * @param dcField name of the DublinCoreElement on which to search
-   * @param search list of values of the DublinCoreElement
-   * @param pageIndex page index
-   * @param pageSize page size
-   * @return a page of digital objects
-   */
-  @GetMapping(path = "/search/dc", produces = MimeTypeUtils.APPLICATION_JSON_VALUE)
-  @ResponseBody
-  @Operation(
-      summary = "Dublin core search based on digital objects and different projects.",
-      description = "Searches for digital objects based on Dublin Core metadata. The search is performed on a specific Dublin Core field with exact match.",
-      responses = {
-          @ApiResponse(responseCode = "200", description = "Digital objects found",
-              content = @Content(mediaType = MimeTypeUtils.APPLICATION_JSON_VALUE)),
-          @ApiResponse(responseCode = "400", description = "Invalid request parameters",
-              content = @Content)
-      }
-  )
-  public Page<DigitalObjectListItemView> searchDigitalObjectsViaDublinCoreExactMatch(
-      @RequestParam Set<String> projectAbbrs,
-      @RequestParam String dcField,
-      @RequestParam @NotEmpty List<String> search,
-      // for pagination
-      @RequestParam(defaultValue = "0") int pageIndex,
-      @RequestParam(defaultValue = "20") int pageSize
-  ){
-
-    // limit page size
-    if (pageSize >= 100) {
-      pageSize = 100;
-    }
-
-    return digitalObjectService.searchObjectsByDublincCoreTags(
-        projectAbbrs, dcField, search, PageRequest.of(pageIndex, pageSize)
-    );
-
-  }
-
-  /**
-   * Fulltext search over all dublin core fields of a digital object.
-   * @param projects list of project abbreviations
-   * @param dcFields list of DublinCoreElement names
-   * @param search fulltext search string
-   * @param pageIndex page index
-   * @param pageSize page size
-   * @return a page of digital objects
-   */
-  @GetMapping(path = "/search/dc/fulltext", produces = MimeTypeUtils.APPLICATION_JSON_VALUE)
-  @ResponseBody
-  @Operation(
-      summary = "Dublin core fulltext search based on digital objects and multiple projects.",
-      description = "Searches for digital objects based on a fulltext search over all Dublin Core fields. The search is performed on multiple projects and can include multiple Dublin Core fields.",
-      responses = {
-          @ApiResponse(responseCode = "200", description = "Digital objects found",
-              content = @Content(mediaType = MimeTypeUtils.APPLICATION_JSON_VALUE)),
-          @ApiResponse(responseCode = "400", description = "Invalid request parameters",
-              content = @Content)
-      }
-  )
-  public Page<DigitalObjectListItemView> searchDigitalObjectsViaDublinCoreFulltext(
-      @RequestParam Set<String> projects,
-      // dublin core search parameters
-      @RequestParam(
-          required = false,
-          // sets default value empty set
-          defaultValue = ""
-      ) Set<String> dcFields,
-      @RequestParam String search,
-      // for pagination
-      @RequestParam(defaultValue = "0") int pageIndex,
-      @RequestParam(defaultValue = "20") int pageSize
-  ){
-
-    // limit page size
-    if (pageSize >= 100) {
-      pageSize = 100;
-    }
-
-    return digitalObjectService.searchByDCFulltext(
-        projects,
-        dcFields,
-        search,
-        PageRequest.of(pageIndex, pageSize)
-    );
-
-  }
 
 }
--- src/test/java/org/zim/gamsapi/Project/ProjectControllerIT.java
+++ src/test/java/org/zim/gamsapi/Project/ProjectControllerIT.java
@@ -1,38 +1,35 @@
 package org.zim.gamsapi.Project;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.auditing.AuditingHandler;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.zim.gamsapi.DigitalObject.DigitalObject;
-import org.zim.gamsapi.DigitalObject.DublinCoreEntry.DublinCoreEntry;
-import org.zim.gamsapi.DigitalObject.DublinCoreEntry.IDublinCoreEntryRepository;
 import org.zim.gamsapi.DigitalObject.IDigitalObjectRepository;
 import org.zim.gamsapi.IntegrationTest;
 import org.zim.gamsapi.Project.interfaces.IProjectRepository;
 import org.zim.gamsapi.enums.TestDigitalObject;
-import org.zim.gamsapi.enums.TestDublinCoreEntry;
 import org.zim.gamsapi.enums.TestProject;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
-import java.util.Set;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @AutoConfigureMockMvc(addFilters = false)
 public class ProjectControllerIT extends IntegrationTest {
 
+  @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
   @Autowired
   private MockMvc mockMvc;
 
@@ -42,13 +39,11 @@ public class ProjectControllerIT extends IntegrationTest {
   @Autowired
   private IDigitalObjectRepository digitalObjectRepository;
 
-  @Autowired
-  private IDublinCoreEntryRepository dublinCoreEntryRepository;
 
   // disables auditing
   // (necessary -> otherwise the createdBy fields etc. from Project need to be filled)
   // this auditing / security test is done in a separate test
-  @MockBean
+  @MockitoBean
   private AuditingHandler auditingHandler;
 
 
@@ -347,134 +342,5 @@ public class ProjectControllerIT extends IntegrationTest {
 
   }
 
-
-  @Nested
-  public class DublinCoreSearch {
-
-    Project testProject = TestProject.generate();
-    DigitalObject testDigitalObject = TestDigitalObject.generate();
-    DublinCoreEntry testDublinCoreEntry = TestDublinCoreEntry.generate(testDigitalObject.getId());
-
-    final String SEARCH_URL_TEMPLATE = "/api/v1/projects/search/dc?projectAbbrs=%s&dcField=%s&search=%s";
-
-    @BeforeEach
-    public void setup() {
-      projectRepository.save(testProject);
-      digitalObjectRepository.save(testDigitalObject);
-      dublinCoreEntryRepository.save(testDublinCoreEntry);
-    }
-
-    @Test
-    public void GETDublinCoreEntryWithContainsReturnsExpectedTestObject() throws Exception {
-      String requestUrl = String.format(
-          SEARCH_URL_TEMPLATE,
-          testProject.getProjectAbbr(),
-          testDublinCoreEntry.getName(),
-          testDublinCoreEntry.getValue()
-      );
-
-      String response = mockMvc.perform(
-          MockMvcRequestBuilders.get(requestUrl)
-      ).andExpect(status().isOk())
-          .andReturn()
-          .getResponse()
-          .getContentAsString();
-
-      Assertions.assertThat(response)
-          .contains(
-              testDigitalObject.getId(),
-              testDigitalObject.getProject().getProjectAbbr(),
-              testDigitalObject.getBaseMetadata().getTitle(),
-              testDigitalObject.getBaseMetadata().getDescription()
-          );
-
-    }
-
-    @Test
-    public void returnsErrorIfSearchParamsWereNotDefined() throws Exception {
-      final String MALFORMED_URL = "/api/v1/projects/search/dc?projectAbbrs=%s";
-      mockMvc.perform(
-          MockMvcRequestBuilders.get(MALFORMED_URL)
-      ).andExpect(
-          status().isBadRequest()
-      );
-
-
-    }
-
-    @Nested
-    public class DCFulltextSearch {
-
-      final String FULLTEXT_SEARCH_URL_TEMPLATE = "/api/v1/projects/search/dc/fulltext?projects=%s&search=%s";
-
-      @Test
-      public void findsExpectedTestObjectJSON_whenSearchingOverAllDCFields() throws Exception {
-
-        // arbitrary substring of test data
-        final String TEST_FULLTEXT_QUERY = TestDublinCoreEntry.VALUE.getValue().substring(0,3);
-
-        String requestUrl = String.format(
-            FULLTEXT_SEARCH_URL_TEMPLATE,
-            testProject.getProjectAbbr(),
-            TEST_FULLTEXT_QUERY
-        );
-        String response = mockMvc.perform(
-                MockMvcRequestBuilders.get(requestUrl)
-            )
-            .andExpect(status().isOk())
-            .andReturn()
-            .getResponse()
-            .getContentAsString();
-
-
-        Assertions.assertThat(response)
-            .contains(
-                testDigitalObject.getId(),
-                testDigitalObject.getProject().getProjectAbbr(),
-                testDigitalObject.getBaseMetadata().getTitle(),
-                testDigitalObject.getBaseMetadata().getDescription()
-            );
-
-      }
-
-      @Test
-      public void findsNothingWhenRestrictedToANonOccuringDCField() throws Exception {
-
-        // arbitrary substring of test data
-        final String TEST_FULLTEXT_QUERY = TestDublinCoreEntry.VALUE.getValue().substring(0,3);
-        final Set<String> TEST_NON_OCCURING_DC_FIELDS = Set.of("type", "format");
-
-        String REQUEST_URL = String.format(
-            "/api/v1/projects/search/dc/fulltext?projects=%s&search=%s",
-            testProject.getProjectAbbr(),
-            TEST_FULLTEXT_QUERY
-        );
-
-        // add dcFields to request url
-        REQUEST_URL += TEST_NON_OCCURING_DC_FIELDS.stream()
-            .reduce("", (acc, field) -> acc + "&dcFields=" + field);
-
-        String response = mockMvc.perform(
-                MockMvcRequestBuilders.get(REQUEST_URL)
-            )
-            .andExpect(status().isOk())
-            .andReturn()
-            .getResponse()
-            .getContentAsString();
-
-
-        Assertions.assertThat(response)
-            .doesNotContain(
-                testDigitalObject.getId(),
-                testDigitalObject.getProject().getProjectAbbr(),
-                testDigitalObject.getBaseMetadata().getTitle(),
-                testDigitalObject.getBaseMetadata().getDescription()
-            );
-
-      }
-
-    }
-
-  }
 
 }
