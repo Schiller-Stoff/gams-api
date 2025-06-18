e05f656ad9961070e9c84fed52d27cb81d67ea5a
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2025-06-18 12:05:33 +0200

    extracted format parameter to static final variable
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/219

--- src/main/java/org/zim/gamsapi/System/config/ContentNegotiationConfig.java
+++ src/main/java/org/zim/gamsapi/System/config/ContentNegotiationConfig.java
@@ -12,13 +12,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class ContentNegotiationConfig implements WebMvcConfigurer {
 
+  public static final String FORMAT_URL_PARAMETER = "format";
+
+
   @Override
   public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
     configurer
         // Enable path extension negotiation (e.g., /api/v1/projects.xml)
         //.favorPathExtension(false) // Deprecated in Spring Boot 3.x, use Accept header
         .favorParameter(true)      // Enable format parameter (?format=xml)
-        .parameterName("format")
+        .parameterName(FORMAT_URL_PARAMETER)
         .ignoreAcceptHeader(false) // Don't ignore Accept header
         .useRegisteredExtensionsOnly(false)
         .defaultContentType(MediaType.APPLICATION_JSON)
