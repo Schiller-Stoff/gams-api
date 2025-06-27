2e2e976b9ed78a9e201ee6c5882e397779110f0a
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2025-06-27 11:52:30 +0200

    supressed unncessary IDE warning
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/222

--- src/test/java/org/zim/gamsapi/System/security/AuthorizationIT.java
+++ src/test/java/org/zim/gamsapi/System/security/AuthorizationIT.java
@@ -25,6 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class AuthorizationIT extends IntegrationTest {
 
 
+  @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
   @Autowired
   private MockMvc mockMvc;
 
