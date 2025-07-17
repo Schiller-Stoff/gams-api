abfbcea42fd01feb216685ebac5835f7631fa752
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2025-07-17 08:34:45 +0200

    removed unused import
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/203

--- src/main/java/org/zim/gamsapi/Project/ProjectController.java
+++ src/main/java/org/zim/gamsapi/Project/ProjectController.java
@@ -26,7 +26,6 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.util.List;
 import java.util.Optional;
 
 @Slf4j
