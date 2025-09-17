16130710eed53a9d46491158a6208dc9e64231e4
Sebastian Schiller-Stoff <sebastian.stoff@uni-graz.at>
2025-09-17 14:43:01 +0200

    added test values for bag-info file
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/245

--- src/test/java/org/zim/gamsapi/TestUtilities/TestBag.java
+++ src/test/java/org/zim/gamsapi/TestUtilities/TestBag.java
@@ -20,5 +20,11 @@ public class TestBag {
     return new ClassPathResource(FOLDER_LOCATION).getFile();
   }
 
-
+  public static class TestBagInfo {
+    public static final String BAGGING_DATE = "2025-08-19";
+    public static final String BAGGING_TIME = "12:07:07 UTC";
+    public static final String PAYLOAD_OXUM = "1140704.5";
+    public static final String CONTACT_EMAIL = "dh@uni-graz.at";
+    public static final String EXTERNAL_DESCRIPTION = "Test bag for gamsapi tests";
+  }
 }
