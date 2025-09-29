1642d9fff709fd5fd96d3af62e862f407ffb622d
Sebi <SebastianStoff@gmx.at>
2025-09-29 09:59:24 +0200

    added test for the reading of bagSipJson
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/245

--- src/test/java/org/zim/gamsapi/Ingest/utils/Bagit/BagDirectoryReaderTest.java
+++ src/test/java/org/zim/gamsapi/Ingest/utils/Bagit/BagDirectoryReaderTest.java
@@ -1,9 +1,9 @@
 package org.zim.gamsapi.Ingest.utils.Bagit;
 
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.TestInstance;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
+import org.zim.gamsapi.Ingest.utils.Bagit.mapping.BagSipJson;
 import org.zim.gamsapi.TestUtilities.TestBag;
 import org.zim.gamsapi.UnitTest;
 
@@ -26,6 +26,60 @@ public class BagDirectoryReaderTest extends UnitTest {
   @Nested
   public class ReadSIPJson {
 
+      BagSipJson  bagSipJson;
+
+      @BeforeEach
+      public void setup(){
+          bagSipJson = BagDirectoryReader.extractAndValidateSipJson(bag.getBAG_DIR_PATH());
+      }
+
+      @Test
+      public void bagSipJsonIsNotNull(){
+          Assertions.assertThat(bagSipJson).isNotNull();
+      }
+
+      @Test
+      public void containsExpectedNumberOfContentFiles(){
+          Assertions.assertThat(bagSipJson.getContentFiles().size())
+                  .isEqualTo(5);
+      }
+
+      @Test
+      public void createsExpectedBagSipJsonObject(){
+
+          var bagSipJson = BagDirectoryReader.extractAndValidateSipJson(bag.getBAG_DIR_PATH());
+
+          Assertions.assertThat(bagSipJson.getCreator())
+                  .isEqualTo(TestBag.TestBagSipJson.CREATOR);
+
+          Assertions.assertThat(bagSipJson.getId())
+                  .isEqualTo(TestBag.TestBagSipJson.REC_ID);
+
+          Assertions.assertThat(bagSipJson.getFunder())
+                  .isEqualTo(TestBag.TestBagSipJson.FUNDER);
+
+          Assertions.assertThat(bagSipJson.getProject())
+                  .isEqualTo(TestBag.TestBagSipJson.PROJECT);
+
+          Assertions.assertThat(bagSipJson.getObjectType())
+                  .isEqualTo(TestBag.TestBagSipJson.OBJECT_TYPE);
+
+          Assertions.assertThat(bagSipJson.getDescription())
+                  .isEqualTo(TestBag.TestBagSipJson.DESCRIPTION);
+
+          Assertions.assertThat(bagSipJson.getRights())
+                    .isEqualTo(TestBag.TestBagSipJson.RIGHTS);
+
+          Assertions.assertThat(bagSipJson.getPublisher())
+                  .isEqualTo(TestBag.TestBagSipJson.PUBLISHER);
+
+            Assertions.assertThat(bagSipJson.getTitle())
+                    .isEqualTo(TestBag.TestBagSipJson.TITLE);
+
+            Assertions.assertThat(bagSipJson.getMainResource())
+                    .isEqualTo(TestBag.TestBagSipJson.MAIN_RESOURCE);
+
+      }
 
 
   }
