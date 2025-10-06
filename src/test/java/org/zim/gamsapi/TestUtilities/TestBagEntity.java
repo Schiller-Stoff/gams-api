5dfd2a885db0ce6a78ca1a0b12817ee26630a947
Sebi <SebastianStoff@gmx.at>
2025-10-06 13:29:13 +0200

    introduced test variables for TestBagEntity
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/271

--- src/test/java/org/zim/gamsapi/TestUtilities/TestBagEntity.java
+++ src/test/java/org/zim/gamsapi/TestUtilities/TestBagEntity.java
@@ -3,9 +3,20 @@ package org.zim.gamsapi.TestUtilities;
 import org.zim.gamsapi.DigitalObject.DigitalObject;
 import org.zim.gamsapi.Ingest.BagEntity;
 
+import java.time.Instant;
+
 public class TestBagEntity {
 
     public static final String ID = TestDigitalObject.DIGITAL_OBJECT_ID.getValue();
+    public static final String CREATED_BY = TestBag.TestBagSipJson.CREATED_BY;
+    public static final String SCHEMA = TestBag.TestBagSipJson.SCHEMA;
+    public static final String SOURCE = TestBag.TestBagSipJson.SOURCE;
+    public static final String EXTERNAL_DESCRIPTION = TestBag.TestBagInfo.EXTERNAL_DESCRIPTION;
+    public static final Float PAYLOAD_OXUM = TestBag.TestBagInfo.PAYLOAD_OXUM;
+    public static final String BAG_VERSION = TestBag.BagitTxt.BAGIT_VERSION;
+    public static final String TAG_FILE_CHARACTER_ENCODING = TestBag.BagitTxt.TAG_FILE_CHARACTER_ENCODING;
+    public static final Instant BAGGING_TIMESTAMP = TestBag.TestBagInfo.BAGGING_TIMESTAMP;
+    public static final String CONTACT_EMAIL = TestBag.TestBagInfo.CONTACT_EMAIL;
 
     /**
      * Generates a BagEntity for testing purposes.
@@ -15,15 +26,15 @@ public class TestBagEntity {
     public static BagEntity generate(DigitalObject digitalObject){
         return BagEntity.builder()
                 .digitalObject(digitalObject)
-                .createdBy(TestBag.TestBagSipJson.CREATED_BY)
-                .schema(TestBag.TestBagSipJson.SCHEMA)
-                .source(TestBag.TestBagSipJson.SOURCE)
-                .externalDescription(TestBag.TestBagInfo.EXTERNAL_DESCRIPTION)
-                .baggingTimeStamp(TestBag.TestBagInfo.BAGGING_TIMESTAMP)
-                .contactMail(TestBag.TestBagInfo.CONTACT_EMAIL)
-                .payloadOxum(TestBag.TestBagInfo.PAYLOAD_OXUM)
-                .bagVersion(TestBag.BagitTxt.BAGIT_VERSION)
-                .tagFileCharacterEncoding(TestBag.BagitTxt.TAG_FILE_CHARACTER_ENCODING)
+                .createdBy(TestBagEntity.CREATED_BY)
+                .schema(TestBagEntity.SCHEMA)
+                .source(TestBagEntity.SOURCE)
+                .externalDescription(TestBagEntity.EXTERNAL_DESCRIPTION)
+                .baggingTimeStamp(TestBagEntity.BAGGING_TIMESTAMP)
+                .contactMail(TestBagEntity.CONTACT_EMAIL)
+                .payloadOxum(TestBagEntity.PAYLOAD_OXUM)
+                .bagVersion(TestBagEntity.BAG_VERSION)
+                .tagFileCharacterEncoding(TestBagEntity.TAG_FILE_CHARACTER_ENCODING)
                 .build();
     }
 
