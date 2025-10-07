d243f4f67aeb3b7eae7bbe68ef96fe56cff61e47
Sebi <SebastianStoff@gmx.at>
2025-10-07 13:37:05 +0200

    added from conversion for BagData
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/272

--- src/main/java/org/zim/gamsapi/Ingest/utils/Bagit/BagData.java
+++ src/main/java/org/zim/gamsapi/Ingest/utils/Bagit/BagData.java
@@ -4,6 +4,10 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
+import org.zim.gamsapi.Datastream.Datastream;
+import org.zim.gamsapi.DigitalObject.DigitalObject;
+import org.zim.gamsapi.Ingest.IngestRecord;
+
 import java.util.HashSet;
 import java.util.Set;
 
@@ -64,5 +68,36 @@ public class BagData {
 
   @NotEmpty
   private String source;
+
+  public static BagData from(DigitalObject digitalObject, Set<Datastream> datastreams, IngestRecord ingestRecord){
+
+      Set<BagFile> contentFiles = new HashSet<>();
+      datastreams.forEach(datastream -> {
+          BagFile bagFile = BagFile.from(datastream);
+          contentFiles.add(bagFile);
+      });
+
+      return BagData.builder()
+              .id(digitalObject.getId())
+              .project(digitalObject.getProject().getProjectAbbr())
+              .title(digitalObject.getBaseMetadata().getTitle())
+              .objectType(digitalObject.getObjectType())
+              .description(digitalObject.getBaseMetadata().getDescription())
+              .creator(digitalObject.getBaseMetadata().getCreator())
+              .rights(digitalObject.getBaseMetadata().getRights())
+              .publisher(digitalObject.getPublisher())
+              .funder(digitalObject.getFunder())
+              .mainResource(digitalObject.getMainResource())
+              .contentFiles(contentFiles)
+              // TODO what is with this types?
+              .types(new HashSet<>())
+              .md5Checksum(digitalObject.getBaseMetadata().getMd5Checksum())
+              .sha512Checksum(digitalObject.getBaseMetadata().getSha512Checksum())
+              .schema(ingestRecord.getBagSchema())
+              .createdBy(ingestRecord.getBagCreatedBy())
+              .source(ingestRecord.getBagSource())
+              .build();
+
+  }
 
 }
