701c1a605caf8331ccb33ffe031234918d2a4af9
Sebi <SebastianStoff@gmx.at>
2025-11-03 11:19:07 +0100

    added link to facet search on sidebar
    https://zimlab.uni-grazhttps://zimlab.uni-graz.at/gams5/gams-management/gams-cms/gams-api/-/issues/15

--- src/main/java/org/ddh/gamsapi/application/Integration/BaseSearch/Facet/FacetController.java
+++ src/main/java/org/ddh/gamsapi/application/Integration/BaseSearch/Facet/FacetController.java
@@ -18,7 +18,6 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.HashMap;
