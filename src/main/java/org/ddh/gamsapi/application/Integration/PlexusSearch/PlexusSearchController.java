c1c4e427b3e71035e9aa0fa8bc9ebccc9b678ddd
Sebastian Schiller-Stoff <sebi.stoff@gmail.com>
2025-11-11 14:29:05 +0100

    plexus search indexing + deletion is now available via controller endpoints
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/281

---
+++ src/main/java/org/ddh/gamsapi/application/Integration/PlexusSearch/PlexusSearchController.java
@@ -0,0 +1,49 @@
+package org.ddh.gamsapi.application.Integration.PlexusSearch;
+
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.ddh.gamsapi.infrastructure.System.config.OpenAPIConfig;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@Tag(name = OpenAPIConfig.INTEGRATION_TAG, description = OpenAPIConfig.INTEGRATION_TAG_DESCRIPTION)
+public class PlexusSearchController {
+
+  public static final String PLEXUS_SEARCH_GET_PATH = "/api/v1/integration/plexus-search";
+
+  public static final String PLEXUS_SEARCH_MANAGEMENT_PATH = PLEXUS_SEARCH_GET_PATH + "/projects/{projectAbbr}/objects";
+
+  public static final String PLEXUS_SEARCH_SINGLE_OBJECT_MANAGEMENT_PATH = PLEXUS_SEARCH_MANAGEMENT_PATH + "/{id}";
+
+  private final PlexusSearchService plexusSearchService;
+
+  @Operation(
+      summary = "Add all project objects to external plexus-search service",
+      description = "This endpoint indexes all objects of a project in the plexus-search service."
+  )
+  @PostMapping(PLEXUS_SEARCH_MANAGEMENT_PATH)
+  public void indexProjectObjects(@PathVariable String projectAbbr){
+    log.debug("*** Trying to index project objects");
+    plexusSearchService.indexObjects(projectAbbr);
+  }
+
+
+  @Operation(
+      summary = "Delete all project objects from external plexus-search service",
+      description = "This endpoint deletes all objects of a project from the plexus-search service."
+  )
+  @DeleteMapping(PLEXUS_SEARCH_MANAGEMENT_PATH)
+  public void deleteProjectObjects(@PathVariable String projectAbbr){
+    log.trace("*** Trying to delete project objects");
+    plexusSearchService.deleteIndexedObjects(projectAbbr);
+  }
+
+}
---
+++ src/test/java/org/ddh/gamsapi/application/Integration/PlexusSearch/PlexusSearchControllerIT.java
@@ -0,0 +1,148 @@
+package org.ddh.gamsapi.application.Integration.PlexusSearch;
+
+import org.assertj.core.api.Assertions;
+import org.ddh.gamsapi.TestUtilities.TestBag;
+import org.ddh.gamsapi.TestUtilities.TestDigitalObject;
+import org.ddh.gamsapi.TestUtilities.TestProject;
+import org.ddh.gamsapi.application.Ingest.Ingest;
+import org.ddh.gamsapi.application.Ingest.interfaces.IIngestService;
+import org.ddh.gamsapi.application.Ingest.utils.ZipUtils;
+import org.ddh.gamsapi.application.Integration.Common.utils.solr.SolrDocument;
+import org.ddh.gamsapi.application.Integration.Common.utils.solr.SolrGamsCores;
+import org.ddh.gamsapi.application.Integration.CustomSearch.CustomSearchService;
+import org.ddh.gamsapi.application.Integration.SolrIntegrationTest;
+import org.ddh.gamsapi.domain.Project.ProjectBuilder;
+import org.ddh.gamsapi.domain.Project.interfaces.IProjectRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.data.auditing.AuditingHandler;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Set;
+
+@AutoConfigureMockMvc(addFilters = false)
+public class PlexusSearchControllerIT extends SolrIntegrationTest {
+
+
+  @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+  @Autowired
+  private MockMvc mockMvc;
+
+  // disables auditing
+  // (necessary -> otherwise the createdBy fields etc. from Project need to be filled)
+  // this auditing / security test is done in a separate test
+  @MockitoBean
+  private AuditingHandler auditingHandler;
+
+  @Autowired
+  private CustomSearchService customSearchService;
+
+  @Autowired
+  private IIngestService ingestService;
+
+  @Autowired
+  private IProjectRepository projectRepository;
+
+  File bagFile;
+
+
+  @BeforeEach
+  public void setup() throws IOException {
+    bagFile = TestBag.loadFile();
+    projectRepository.save(ProjectBuilder.builder().projectAbbr(TestProject.PROJECT_ABBR.getValue()).build());
+
+    // ingest the bag
+    byte[] zippedBag = ZipUtils.zipDir(bagFile);
+    Ingest ingest = new Ingest();
+    ingest.setZippedBagItFolder(zippedBag);
+    ingest.setProjectAbbr(TestProject.PROJECT_ABBR.getValue());
+    ingestService.ingest(ingest);
+
+  }
+
+  @Nested
+  public class IndexProjectObjects {
+
+    @Test
+    public void indexCreatesAtLeast1SolrDocument() throws Exception {
+      // when
+      mockMvc.perform(
+              org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+                  .post(PlexusSearchController.PLEXUS_SEARCH_MANAGEMENT_PATH,
+                      TestProject.PROJECT_ABBR.getValue())
+          )
+          .andExpect(org.springframework.test.web.servlet.result.MockMvcResultMatchers.status().isOk());
+
+      // then
+      var numDocs = solrClient.countProjectDocuments(
+          SolrGamsCores.PLEXUS_SEARCH_CORE.value,
+          Set.of(TestProject.PROJECT_ABBR.getValue())
+      );
+
+      Assertions.assertThat(numDocs)
+          .withFailMessage("Number of documents returned should be greater than 0")
+          .isGreaterThan(0)
+      ;
+
+    }
+
+
+  }
+
+  @Nested
+  public class DeleteProjectObjects {
+
+    @Test
+    public void deleteRemovesAllSolrDocuments() throws Exception {
+      // given
+      // first index some documents
+      final String TEST_SOLR_DOCUMENT_ID = "test.1111111";
+
+      // first fill data of plexus search core
+      SolrDocument testSolrDocument = new SolrDocument();
+      testSolrDocument.addProperty(PlexusSearchProperties.ENTITY_ID.name, TEST_SOLR_DOCUMENT_ID);
+      testSolrDocument.addProperty(PlexusSearchProperties.ENTITY_PROJECT_ABBR.name, TestProject.PROJECT_ABBR.getValue());
+      testSolrDocument.addProperty(PlexusSearchProperties.ENTITY_OBJECT_ID.name, TestDigitalObject.DIGITAL_OBJECT_ID.getValue());
+      // posting data to solr
+      solrClient.post(SolrGamsCores.PLEXUS_SEARCH_CORE.value, testSolrDocument);
+
+      var numDocsAfterIndexing = solrClient.countProjectDocuments(
+          SolrGamsCores.PLEXUS_SEARCH_CORE.value,
+          Set.of(TestProject.PROJECT_ABBR.getValue())
+      );
+
+      Assertions.assertThat(numDocsAfterIndexing)
+          .withFailMessage("Number of documents after indexing should be greater than 0")
+          .isGreaterThan(0)
+      ;
+
+      // when
+      mockMvc.perform(
+              org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+                  .delete(PlexusSearchController.PLEXUS_SEARCH_MANAGEMENT_PATH,
+                      TestProject.PROJECT_ABBR.getValue())
+          )
+          .andExpect(org.springframework.test.web.servlet.result.MockMvcResultMatchers.status().isOk());
+
+      // then
+      var numDocsAfterDeletion = solrClient.countProjectDocuments(
+          SolrGamsCores.PLEXUS_SEARCH_CORE.value,
+          Set.of(TestProject.PROJECT_ABBR.getValue())
+      );
+
+      Assertions.assertThat(numDocsAfterDeletion)
+          .withFailMessage("Number of documents after deletion should be equal to 0")
+          .isEqualTo(0)
+      ;
+
+    }
+
+  }
+
+}
