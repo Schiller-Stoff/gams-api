2e3aa5875dc688bdaac87337041242678e7e5a57
Sebi <SebastianStoff@gmx.at>
2025-11-12 12:10:54 +0100

    added unittest to build solr plexus search urls
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/281

--- src/test/java/org/ddh/gamsapi/application/Integration/PlexusSearch/PlexusSearchSolrQueryBuilderTest.java
+++ src/test/java/org/ddh/gamsapi/application/Integration/PlexusSearch/PlexusSearchSolrQueryBuilderTest.java
@@ -9,6 +9,7 @@ import org.ddh.gamsapi.application.Integration.PlexusSearch.dto.PlexusSearchQuer
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Set;
 
 public class PlexusSearchSolrQueryBuilderTest extends UnitTest {
@@ -34,8 +35,6 @@ public class PlexusSearchSolrQueryBuilderTest extends UnitTest {
           Set.of(TestProject.PROJECT_ABBR.getValue())
       );
 
-      System.out.println(generatedSolrUrl);
-
       final String expectedContainedQuery = SolrUrlBuilder.urlEncode(EXACT_MATCH_QUERY);
       Assertions.assertThat(generatedSolrUrl)
         .contains(expectedContainedQuery);
@@ -47,7 +46,7 @@ public class PlexusSearchSolrQueryBuilderTest extends UnitTest {
           )
       );
       Assertions.assertThat(generatedSolrUrl)
-          .contains(expectedContainedProjectAbbrFilter);
+          .contains("fq=" + expectedContainedProjectAbbrFilter);
 
 
       final String expectedStart = String.format("start=%s", requestDto.getStart());
@@ -67,6 +66,107 @@ public class PlexusSearchSolrQueryBuilderTest extends UnitTest {
       final String expectedSort = String.format("sort=%s", SolrUrlBuilder.urlEncode(requestDto.getSort()));
       Assertions.assertThat(generatedSolrUrl)
           .contains(expectedSort);
+
+    }
+
+    @Test
+    public void buildsExpectedComplexUrlContainsExpectedUrlParameterValues() {
+
+      final String EXACT_MATCH_QUERY = String.format("%s:%s",
+          PlexusSearchProperties.ENTITY_PROJECT_ABBR.name,
+          TestProject.PROJECT_ABBR.getValue()
+      );
+
+      PlexusSearchQueryRequestDto requestDto = PlexusSearchQueryRequestDto.builder()
+          .query(EXACT_MATCH_QUERY)
+          .sort(PlexusSearchProperties.ENTITY_OBJECT_ID.name + " desc")
+          .debug(true)
+          .facetLimit(50)
+          .filterQueries(
+              List.of(
+                  String.format("%s:[%d TO %d]",
+                      PlexusSearchProperties.ENTITY_ID.name,
+                      100,
+                      200
+                  )
+              )
+          )
+          .facetFields(
+              List.of(
+                  PlexusSearchProperties.ENTITY_PROJECT_ABBR.name,
+                  PlexusSearchProperties.ENTITY_ID.name
+              )
+          )
+          .highlight(true)
+          .highlightSnippetSize(5)
+          .highlightFields(
+              List.of(
+                  PlexusSearchProperties.ENTITY_OBJECT_ID.name
+              )
+          )
+          .fields(
+              List.of(
+                  PlexusSearchProperties.ENTITY_ID.name,
+                  PlexusSearchProperties.ENTITY_PROJECT_ABBR.name,
+                  PlexusSearchProperties.ENTITY_OBJECT_ID.name
+              )
+          )
+          .build();
+
+      var generatedSolrUrl = PlexusSearchSolrQueryBuilder.buildSolrQueryUrl(
+          SolrGamsCores.PLEXUS_SEARCH_CORE.value,
+          requestDto,
+          Set.of(TestProject.PROJECT_ABBR.getValue())
+      );
+
+      final String EXPECTED_FL_PARAM = String.format("fl=%s",
+          String.join(",", requestDto.getFields())
+      );
+      Assertions.assertThat(generatedSolrUrl)
+          .contains(EXPECTED_FL_PARAM);
+
+      final String EXPECTED_FILTER_QUERY_PARAM = SolrUrlBuilder.urlEncode(
+          requestDto.getFilterQueries().get(0) // only one in this test case
+      );
+      Assertions.assertThat(generatedSolrUrl)
+          .contains("fq=" + EXPECTED_FILTER_QUERY_PARAM);
+
+      final String EXPECTED_FACET_FIELD_1 = SolrUrlBuilder.urlEncode(
+          requestDto.getFacetFields().get(0)
+      );
+      Assertions.assertThat(generatedSolrUrl)
+          .contains("facet.field=" + EXPECTED_FACET_FIELD_1);
+
+      final String EXPECTED_HL_PARAM = String.format("hl=%s",
+          requestDto.getHighlight()
+      );
+      Assertions.assertThat(generatedSolrUrl)
+          .contains(EXPECTED_HL_PARAM);
+
+      final String EXPECTED_HL_FIELD = SolrUrlBuilder.urlEncode(
+          requestDto.getHighlightFields().get(0) // only one in this test case
+      );
+      Assertions.assertThat(generatedSolrUrl)
+          .contains("hl.fl=" + EXPECTED_HL_FIELD);
+
+      final String EXPECTED_HL_SNIPPET_SIZE = String.format("hl.snippets=%s",
+          requestDto.getHighlightSnippetSize()
+      );
+      Assertions.assertThat(generatedSolrUrl)
+          .contains(EXPECTED_HL_SNIPPET_SIZE);
+
+      final String EXPECTED_FACET_LIMIT = String.format("facet.limit=%s",
+          requestDto.getFacetLimit()
+      );
+      Assertions.assertThat(generatedSolrUrl)
+          .contains(EXPECTED_FACET_LIMIT);
+
+      final String EXPECTED_SORT = String.format("sort=%s",
+          SolrUrlBuilder.urlEncode(requestDto.getSort())
+      );
+      Assertions.assertThat(generatedSolrUrl)
+          .contains(EXPECTED_SORT);
+
 
     }
 
