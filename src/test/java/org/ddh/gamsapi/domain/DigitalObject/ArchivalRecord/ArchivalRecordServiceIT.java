1936b025c81a55a3620e47fcc9ba1a3ac19efd5a
Sebi <SebastianStoff@gmx.at>
2026-01-22 13:08:26 +0100

    added test for saving an archival record via the service method
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/310

--- src/test/java/org/ddh/gamsapi/domain/DigitalObject/ArchivalRecord/ArchivalRecordServiceIT.java
+++ src/test/java/org/ddh/gamsapi/domain/DigitalObject/ArchivalRecord/ArchivalRecordServiceIT.java
@@ -2,6 +2,7 @@ package org.ddh.gamsapi.domain.DigitalObject.ArchivalRecord;
 
 import org.assertj.core.api.Assertions;
 import org.ddh.gamsapi.IntegrationTest;
+import org.ddh.gamsapi.TestUtilities.TestArchivalRecord;
 import org.ddh.gamsapi.TestUtilities.TestDataBuilder;
 import org.ddh.gamsapi.TestUtilities.TestDataSet;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,6 +12,8 @@ import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.auditing.AuditingHandler;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import java.time.Instant;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ArchivalRecordServiceIT extends IntegrationTest {
@@ -25,15 +28,15 @@ public class ArchivalRecordServiceIT extends IntegrationTest {
   @Autowired
   private TestDataBuilder testDataBuilder;
 
+  private TestDataSet testDataSet;
+
+  @BeforeEach
+  public void setup() {
+    testDataSet = testDataBuilder.buildTestDataSet();
+  }
+
   @Nested
   public class FindForObject {
-
-    private TestDataSet testDataSet;
-
-    @BeforeEach
-    public void setup() {
-      testDataSet = testDataBuilder.buildTestDataSet();
-    }
 
     @Test
     public void foundArchivalRecordIsNotNullAndIsNotEmpty(){
@@ -43,6 +46,34 @@ public class ArchivalRecordServiceIT extends IntegrationTest {
       Assertions.assertThat(foundRecord).hasSize(1);
     }
 
+    @Test
+    public void findsExpectedArchivalRecord(){
+      var expectedRecords = archivalRecordService.findForObject(testDataSet.digitalObject().getId());
+      Assertions.assertThat(
+          expectedRecords.getFirst().getId()
+      ).isEqualTo(testDataSet.archivalRecord().getId());
+    }
+
+  }
+
+
+  @Nested
+  public class Save {
+
+    @Test
+    public void savesExpectedAdditionalArchivalRecord(){
+
+      ArchivalRecordCreateDto archivalRecordCreateDto = new ArchivalRecordCreateDto();
+      archivalRecordCreateDto.setDigitalObjectId(testDataSet.digitalObject().getId());
+      archivalRecordCreateDto.setPid(testDataSet.archivalRecord().getPid());
+      archivalRecordCreateDto.setTimeStamp(Instant.now());
+      archivalRecordService.save(archivalRecordCreateDto);
+
+      var foundRecords = archivalRecordService.findForObject(testDataSet.digitalObject().getId());
+
+      Assertions.assertThat(foundRecords).hasSize(2);
+
+    }
 
   }
 
