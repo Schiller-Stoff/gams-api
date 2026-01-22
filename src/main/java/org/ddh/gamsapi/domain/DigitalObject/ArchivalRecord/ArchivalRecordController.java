3e67137072b5e23b9f22f569561f9ccedafb2018
Sebi <SebastianStoff@gmx.at>
2026-01-22 14:47:04 +0100

    added GET all archival records for a digital object
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/310

--- src/main/java/org/ddh/gamsapi/domain/DigitalObject/ArchivalRecord/ArchivalRecordController.java
+++ src/main/java/org/ddh/gamsapi/domain/DigitalObject/ArchivalRecord/ArchivalRecordController.java
@@ -1,12 +1,20 @@
 package org.ddh.gamsapi.domain.DigitalObject.ArchivalRecord;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.ddh.gamsapi.domain.Project.interfaces.IProjectService;
 import org.ddh.gamsapi.infrastructure.System.config.OpenAPIConfig;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @RequestMapping(value = { "/api/v1/projects/{projectAbbr}/objects/{id}/archival-records" })
@@ -17,6 +25,24 @@ public class ArchivalRecordController {
 
   private final IArchivalRecordService archivalRecordService;
   private final IProjectService projectService;
+
+  @Operation(
+      summary = "Get archival records",
+      description = "Retrieve the archival records associated with a specific digital object within a project.",
+      responses = {
+          @ApiResponse(responseCode = "200", description = "Successful retrieval of the archival records",
+              content = @Content)
+      }
+  )
+  @RequestMapping(method = RequestMethod.GET)
+  @ResponseBody
+  public List<ArchivalRecordCompactView> findSubmissionRecord(
+      @PathVariable String projectAbbr,
+      @PathVariable String id
+  ) {
+    projectService.verifyProjectAbbrMatchesObjectId(projectAbbr, id);
+    return archivalRecordService.findForObject(id);
+  }
 
 
 }
