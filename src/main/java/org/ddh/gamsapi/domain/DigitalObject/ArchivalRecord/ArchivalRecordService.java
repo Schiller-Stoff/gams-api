3227d50bf7cbcfc293696d6e66be7bbfbac50b9c
Sebi <SebastianStoff@gmx.at>
2026-01-22 13:08:47 +0100

    optimized import
    https://zimlab.uni-graz.at/gams5/production/issue-backlog/-/issues/310

--- src/main/java/org/ddh/gamsapi/domain/DigitalObject/ArchivalRecord/ArchivalRecordService.java
+++ src/main/java/org/ddh/gamsapi/domain/DigitalObject/ArchivalRecord/ArchivalRecordService.java
@@ -3,7 +3,6 @@ package org.ddh.gamsapi.domain.DigitalObject.ArchivalRecord;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.ddh.gamsapi.domain.DigitalObject.DigitalObject;
-import org.ddh.gamsapi.domain.DigitalObject.DigitalObjectBuilder;
 import org.ddh.gamsapi.domain.DigitalObject.utils.exceptions.DigitalObjectNotFoundException;
 import org.ddh.gamsapi.domain.DigitalObject.utils.interfaces.IDigitalObjectRepository;
 import org.springframework.stereotype.Service;
@@ -28,9 +27,9 @@ public class ArchivalRecordService implements IArchivalRecordService {
 
     final String DIGITAL_OBJECT_ID = archivalRecordCreateDto.getDigitalObjectId();
 
-    if(!digitalObjectRepository.existsById(DIGITAL_OBJECT_ID)){
+    if (!digitalObjectRepository.existsById(DIGITAL_OBJECT_ID)) {
       throw new DigitalObjectNotFoundException(
-          "Cannot save archival record " + archivalRecordCreateDto  + ". The digital object with id does not exist: " + DIGITAL_OBJECT_ID
+          "Cannot save archival record " + archivalRecordCreateDto + ". The digital object with id does not exist: " + DIGITAL_OBJECT_ID
       );
     }
 
