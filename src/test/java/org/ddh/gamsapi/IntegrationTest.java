dfd6935bed096789d825a2ae3ee5c2f3146562e7
Sebi <SebastianStoff@gmx.at>
2026-01-23 14:53:10 +0100

    upgraded to same version as used in production
    https://zimlab.uni-graz.at/gams5/gams-management/gams-cms/gams-api/-/issues/17

--- src/test/java/org/ddh/gamsapi/IntegrationTest.java
+++ src/test/java/org/ddh/gamsapi/IntegrationTest.java
@@ -38,7 +38,7 @@ public abstract class IntegrationTest {
 
   // setup of test-containers: https://java.testcontainers.org/test_framework_integration/manual_lifecycle_control/
   static {
-    postgres = new PostgreSQLContainer<>("postgres:13-alpine");
+    postgres = new PostgreSQLContainer<>("postgres:15.3-alpine");
     postgres.start();
   }
 
