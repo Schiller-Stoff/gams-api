51808a48f639f14edca7dbf94db771f6f03454d6
Sebi <SebastianStoff@gmx.at>
2026-02-16 14:52:36 +0100

    corrected test data checksums for unix
    https://zimlab.uni-graz.at/gams5/gams-management/gams-cms/gams-api/-/issues/28

--- src/test/java/org/ddh/gamsapi/application/Ingest/utils/Bagit/BagDirectoryReaderTest.java
+++ src/test/java/org/ddh/gamsapi/application/Ingest/utils/Bagit/BagDirectoryReaderTest.java
@@ -147,7 +147,7 @@ public class BagDirectoryReaderTest extends UnitTest {
       public void createsExpectedSha512ManifestObject() {
           var sha512Manifest = BagDirectoryReader.readSha512ManifestFile(bag.getBAG_DIR_PATH());
           Assertions.assertThat(sha512Manifest.size()).isEqualTo(8);
-          Assertions.assertThat(sha512Manifest.get("data/content/DC.xml")).isEqualTo("098f20b59928b3e671247a90a3a54df58e4b10ae4daf9cd4d9e6ce51c9d455479174de271a271ca5aa33221ba378b40fe9addfa3377b126018792844c7a53d2a");
+          Assertions.assertThat(sha512Manifest.get("data/content/DC.xml")).isEqualTo("f36bbb5ec833b58d1dc7ec4cac08417143883c092d229944c23d60547a9e4c2c8f515b2ea9e59957998d9ccbf5b591f9abb5e9bb03365bd4e484198f7ef5945d");
       }
 
 
@@ -172,7 +172,7 @@ public class BagDirectoryReaderTest extends UnitTest {
     public void createsExpectedMd5ManifestObject() {
         var md5Manifest = BagDirectoryReader.readMd5ManifestFile(bag.getBAG_DIR_PATH());
         Assertions.assertThat(md5Manifest.size()).isEqualTo(8);
-        Assertions.assertThat(md5Manifest.get("data/content/DC.xml")).isEqualTo("721173c0786e2870f0e8d52a2ec17ebc");
+        Assertions.assertThat(md5Manifest.get("data/content/DC.xml")).isEqualTo("310def22018d3a6608f133630748ddfc");
     }
 
   }
