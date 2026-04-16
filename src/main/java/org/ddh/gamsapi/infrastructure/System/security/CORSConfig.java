20d9977dd0952c519023895bc2a4f4e5860d8b91
Sebi <SebastianStoff@gmx.at>
2026-04-16 13:52:26 +0200

    moved cors config to different package
    https://zimlab.uni-graz.at/gams5/gams-management/gams-cms/gams-api/-/work_items/67

--- src/main/java/org/ddh/gamsapi/infrastructure/System/security/CORSConfig.java
+++ src/main/java/org/ddh/gamsapi/infrastructure/System/security/CORSConfig.java
@@ -1,4 +1,4 @@
-package org.ddh.gamsapi.infrastructure.System.config;
+package org.ddh.gamsapi.infrastructure.System.security;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
